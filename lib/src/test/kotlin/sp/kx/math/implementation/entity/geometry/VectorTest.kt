b017b1eeb6f224596d8b4f437ec90c8c02dfd1cf
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-07 13:51:05 +0300

    fix iss/8

--- lib/src/test/kotlin/sp/kx/math/implementation/entity/geometry/VectorTest.kt
+++ lib/src/test/kotlin/sp/kx/math/implementation/entity/geometry/VectorTest.kt
@@ -17,7 +17,16 @@ class VectorTest {
         val start = pointOf(x = 1.0, y = 2.0)
         val finish = pointOf(x = 3.0, y = 4.0)
         val vector = start.toVector(finish = finish)
-        start.assert(vector.start, prefix = "start")
-        finish.assert(vector.finish, prefix = "finish")
+        start.assert(vector.start, prefix = "start.")
+        finish.assert(vector.finish, prefix = "finish.")
+    }
+
+    @Test
+    fun toVectorOffsetTest() {
+        val start = pointOf(x = 1.0, y = 2.0)
+        val offset = offsetOf(dX = 3.0, dY = 4.0)
+        val vector = start.toVector(offset = offset)
+        start.assert(vector.start, prefix = "start.")
+        start.updated(offset).assert(vector.finish, prefix = "finish.")
     }
 }
