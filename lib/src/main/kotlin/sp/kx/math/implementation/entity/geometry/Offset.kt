afca43dbb5d3b3dbdf39b5077429fc5eed6964c1
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-07 14:11:06 +0300

    fix code quality

--- lib/src/main/kotlin/sp/kx/math/implementation/entity/geometry/Offset.kt
+++ lib/src/main/kotlin/sp/kx/math/implementation/entity/geometry/Offset.kt
@@ -1,14 +1,15 @@
 package sp.kx.math.implementation.entity.geometry
 
 import sp.kx.math.foundation.entity.geometry.Offset
+import java.util.Locale
 
 private data class OffsetImpl(
     override val dX: Double,
     override val dY: Double
 ) : Offset {
     override fun toString(): String {
-        val fX = String.format("%.2f", dX)
-        val fY = String.format("%.2f", dY)
+        val fX = String.format(Locale.US, "%.2f", dX)
+        val fY = String.format(Locale.US, "%.2f", dY)
         return "{dX:$fX,dY:$fY}"
     }
 }
--- lib/src/main/kotlin/sp/kx/math/implementation/entity/geometry/Point.kt
+++ lib/src/main/kotlin/sp/kx/math/implementation/entity/geometry/Point.kt
@@ -1,14 +1,15 @@
 package sp.kx.math.implementation.entity.geometry
 
 import sp.kx.math.foundation.entity.geometry.Point
+import java.util.Locale
 
 private data class PointImpl(
     override val x: Double,
     override val y: Double
 ): Point {
     override fun toString(): String {
-        val fX = String.format("%.2f", x)
-        val fY = String.format("%.2f", y)
+        val fX = String.format(Locale.US, "%.2f", x)
+        val fY = String.format(Locale.US, "%.2f", y)
         return "{x:$fX,y:$fY}"
     }
 }
