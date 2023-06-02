7af32265c993cbd2abe5fdbe579e149ce9280281
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-02 19:48:44 +0300

    com.github.kepocnhh:GradleExtension.Core:0.3.0-SNAPSHOT

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,28 @@
+import sp.gx.core.Badge
+import sp.gx.core.GitHub
+import sp.gx.core.Markdown
+import sp.gx.core.Maven
+import sp.gx.core.assemble
+import sp.gx.core.camelCase
+import sp.gx.core.check
+import sp.gx.core.colonCase
+import sp.gx.core.existing
+import sp.gx.core.file
+import sp.gx.core.filled
+import sp.gx.core.kebabCase
 import java.net.URL
 
 version = "0.2.0"
+
+val maven = Maven.Artifact(
+    group = "com.github.kepocnhh",
+    id = rootProject.name,
+)
+
+val gh = GitHub.Repository(
+    owner = "StanleyProjects",
+    name = rootProject.name,
+)
 
 repositories.mavenCentral()
 
@@ -11,6 +33,13 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.jupiter}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.jupiter}")
+}
+
+jacoco.toolVersion = Version.jacoco
+
 tasks.getByName<JavaCompile>("compileJava") {
     targetCompatibility = Version.jvmTarget
 }
@@ -18,12 +47,8 @@ tasks.getByName<JavaCompile>("compileJava") {
 val compileKotlinTask = tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
     kotlinOptions {
         jvmTarget = Version.jvmTarget
-        freeCompilerArgs = freeCompilerArgs + setOf("-module-name", Maven.groupId + ":" + Maven.artifactId)
+        freeCompilerArgs = freeCompilerArgs + setOf("-module-name", colonCase(maven.group, maven.id))
     }
-}
-
-dependencies {
-    testImplementation("junit:junit:${Version.junit}")
 }
 
 tasks.getByName<JavaCompile>("compileTestJava") {
@@ -34,23 +59,32 @@ tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKot
     kotlinOptions.jvmTarget = Version.jvmTarget
 }
 
-val testTask = tasks.getByName<Test>("test")
-
-jacoco {
-    toolVersion = Version.jacoco
+val taskUnitTest = task<Test>("checkUnitTest") {
+    useJUnitPlatform()
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
 }
 
-val testCoverageTask = tasks.getByName<JacocoReport>("jacocoTestReport") {
-    dependsOn(testTask)
+val taskCoverageReport = task<JacocoReport>("assembleCoverageReport") {
+    dependsOn(taskUnitTest)
     reports {
         csv.required.set(false)
         html.required.set(true)
         xml.required.set(false)
     }
+    sourceDirectories.setFrom(file("src/main/kotlin"))
+    classDirectories.setFrom(sourceSets.main.get().output.classesDirs)
+    executionData(taskUnitTest)
+    doLast {
+        val report = buildDir.resolve("reports/jacoco/$name/html/index.html")
+        if (report.exists()) {
+            println("Coverage report: ${report.absolutePath}")
+        }
+    }
 }
 
-tasks.getByName<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
-    dependsOn(testCoverageTask)
+task<JacocoCoverageVerification>("checkCoverage") {
+    dependsOn(taskCoverageReport)
     violationRules {
         rule {
             limit {
@@ -58,13 +92,14 @@ tasks.getByName<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
             }
         }
     }
+    classDirectories.setFrom(taskCoverageReport.classDirectories)
+    executionData(taskCoverageReport.executionData)
 }
 
-setOf("main", "test").forEach {
-    val detektTask = tasks.getByName<io.gitlab.arturbosch.detekt.Detekt>("detekt${it.capitalize()}")
+setOf("main", "test").also { types ->
     val configs = setOf(
-        "common",
         "comments",
+        "common",
         "complexity",
         "coroutines",
         "empty-blocks",
@@ -72,101 +107,145 @@ setOf("main", "test").forEach {
         "naming",
         "performance",
         "potential-bugs",
-        "style"
+        "style",
     ).map { config ->
-        File(rootDir, "buildSrc/src/main/resources/detekt/config/$config.yml").existing()
+        rootDir.resolve("buildSrc/src/main/resources/detekt/config/$config.yml")
+            .existing()
+            .file()
+            .filled()
     }
-    task<io.gitlab.arturbosch.detekt.Detekt>("verifyCodeQuality${it.capitalize()}") {
-        jvmTarget = Version.jvmTarget
-        setSource(files("src/$it/kotlin"))
-        config.setFrom(configs)
-        reports {
-            xml.required.set(false)
-            sarif.required.set(false)
-            txt.required.set(false)
-            html {
-                required.set(true)
-                outputLocation.set(File(buildDir, "reports/analysis/code/quality/$it/html/index.html"))
-            }
-        }
-        classpath.setFrom(detektTask.classpath)
-    }
-}
-
-"unstable".also { variant ->
-    val tag = "${Version.name}-${variant.toUpperCase()}"
-    task<Jar>("assemble${variant.capitalize()}Jar") {
-        dependsOn(compileKotlinTask)
-        archiveBaseName.set(Repository.name)
-        archiveVersion.set(tag)
-        from(compileKotlinTask.destinationDirectory.asFileTree)
-    }
-}
-
-"snapshot".also { variant ->
-    val tag = "${Version.name}-${variant.toUpperCase()}"
-    task<Jar>("assemble${variant.capitalize()}Jar") {
-        dependsOn(compileKotlinTask)
-        archiveBaseName.set(Maven.artifactId)
-        archiveVersion.set(tag)
-        from(compileKotlinTask.destinationDirectory.asFileTree)
-    }
-    task<Jar>("assemble${variant.capitalize()}Source") {
-        archiveBaseName.set(Maven.artifactId)
-        archiveVersion.set(tag)
-        archiveClassifier.set("sources")
-        from("src/main")
-    }
-    task<Jar>("assemble${variant.capitalize()}Pom") {
-        doLast {
-            val parent = File(buildDir, "libs")
-            if (!parent.exists()) parent.mkdirs()
-            val file = File(parent, "${Maven.artifactId}-$tag.pom")
-            if (file.exists()) file.delete()
-            val text = MavenUtil.pom(
-                modelVersion = "4.0.0",
-                groupId = Maven.groupId,
-                artifactId = Maven.artifactId,
-                version = tag,
-                packaging = "jar"
-            )
-            file.writeText(text)
-        }
-    }
-    task<org.jetbrains.dokka.gradle.DokkaTask>("assemble${variant.capitalize()}Documentation") {
-        outputDirectory.set(File(buildDir, "documentation/$variant"))
-        moduleName.set(Maven.artifactId)
-        moduleVersion.set(tag)
-        dokkaSourceSets {
-            named("main") {
-                reportUndocumented.set(false)
-                sourceLink {
-                    val path = "src/main/kotlin"
-                    localDirectory.set(file(path))
-                    remoteUrl.set(URL("${Repository.url()}/tree/$tag/lib/$path"))
+    types.forEach { type ->
+        task<io.gitlab.arturbosch.detekt.Detekt>(camelCase("check", "CodeQuality", type)) {
+            jvmTarget = Version.jvmTarget
+            source = sourceSets.getByName(type).allSource
+            config.setFrom(configs)
+            reports {
+                html {
+                    required.set(true)
+                    outputLocation.set(buildDir.resolve("reports/analysis/code/quality/$type/html/index.html"))
                 }
+                md.required.set(false)
+                sarif.required.set(false)
+                txt.required.set(false)
+                xml.required.set(false)
             }
+            val detektTask = tasks.getByName<io.gitlab.arturbosch.detekt.Detekt>(camelCase("detekt", type))
+            classpath.setFrom(detektTask.classpath)
         }
     }
 }
 
-task<io.gitlab.arturbosch.detekt.Detekt>("verifyDocumentation") {
-    jvmTarget = Version.jvmTarget
-    setSource(files("src/main/kotlin"))
+task<io.gitlab.arturbosch.detekt.Detekt>("checkDocumentation") {
     val configs = setOf(
         "common",
-        "documentation"
+        "documentation",
     ).map { config ->
-        File(rootDir, "buildSrc/src/main/resources/detekt/config/$config.yml").existing()
+        rootDir.resolve("buildSrc/src/main/resources/detekt/config/$config.yml")
+            .existing()
+            .file()
+            .filled()
     }
+    jvmTarget = Version.jvmTarget
+    source = sourceSets.main.get().allSource
     config.setFrom(configs)
     reports {
         html {
             required.set(true)
-            outputLocation.set(File(buildDir, "reports/documentation/html/index.html"))
+            outputLocation.set(buildDir.resolve("reports/analysis/documentation/html/index.html"))
         }
-        xml.required.set(false)
-        txt.required.set(false)
+        md.required.set(false)
         sarif.required.set(false)
+        txt.required.set(false)
+        xml.required.set(false)
+    }
+    val detektTask = tasks.getByName<io.gitlab.arturbosch.detekt.Detekt>("detektMain")
+    classpath.setFrom(detektTask.classpath)
+}
+
+"snapshot".also { variant ->
+    val version = kebabCase(version.toString(), variant.toUpperCase())
+    task<Jar>(camelCase("assemble", variant, "Jar")) {
+        dependsOn(compileKotlinTask)
+        archiveBaseName.set(maven.id)
+        archiveVersion.set(version)
+        from(compileKotlinTask.destinationDirectory.asFileTree)
+    }
+    task<Jar>(camelCase("assemble", variant, "Source")) {
+        archiveBaseName.set(maven.id)
+        archiveVersion.set(version)
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+    task(camelCase("assemble", variant, "Pom")) {
+        doLast {
+            buildDir.resolve("libs/${kebabCase(maven.id, version)}.pom").assemble(
+                Maven.pom(
+                    groupId = maven.group,
+                    artifactId = maven.id,
+                    version = version,
+                    packaging = "jar",
+                ),
+            )
+        }
+    }
+    task(camelCase("assemble", variant, "MavenMetadata")) {
+        doLast {
+            buildDir.resolve("xml/maven-metadata.xml").assemble(
+                Maven.metadata(
+                    groupId = maven.group,
+                    artifactId = maven.id,
+                    version = version,
+                ),
+            )
+        }
+    }
+    task<org.jetbrains.dokka.gradle.DokkaTask>(camelCase("assemble", variant, "Documentation")) {
+        outputDirectory.set(buildDir.resolve("documentation/$variant"))
+        moduleName.set(gh.name)
+        moduleVersion.set(version)
+        dokkaSourceSets.getByName("main") {
+            val path = "src/$name/kotlin"
+            reportUndocumented.set(false)
+            sourceLink {
+                localDirectory.set(file(path))
+                val url = GitHub.url(gh.owner, gh.name)
+                remoteUrl.set(URL("$url/tree/${moduleVersion.get()}/lib/$path"))
+            }
+            jdkVersion.set(Version.jvmTarget.toInt())
+        }
+    }
+    task(camelCase("assemble", variant, "Metadata")) {
+        doLast {
+            buildDir.resolve("yml/metadata.yml").assemble(
+                """
+                    repository:
+                     owner: '${gh.owner}'
+                     name: '${gh.name}'
+                    version: '$version'
+                """.trimIndent(),
+            )
+        }
+    }
+    task(camelCase("check", variant, "Readme")) {
+        doLast {
+            val badge = Markdown.image(
+                text = "version",
+                url = Badge.url(
+                    label = "version",
+                    message = version,
+                    color = "2962ff",
+                ),
+            )
+            val expected = setOf(
+                badge,
+                Markdown.link("Maven", Maven.Snapshot.url(maven.group, maven.id, version)),
+                Markdown.link("Documentation", URL(GitHub.pages(gh.owner, gh.name).toString() + "/doc/$version")), // todo resolve
+                "implementation(\"${maven.group}:${maven.id}:$version\")",
+            )
+            rootDir.resolve("README.md").check(
+                expected = expected,
+                report = buildDir.resolve("reports/analysis/readme/index.html"),
+            )
+        }
     }
 }
