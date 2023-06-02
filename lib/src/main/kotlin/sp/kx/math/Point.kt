8b62f7b208b55aa21329be945b764ef253189c0d
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-02 20:27:39 +0300

    fix code quality

--- lib/src/main/kotlin/sp/kx/math/Point.kt
+++ lib/src/main/kotlin/sp/kx/math/Point.kt
@@ -2,12 +2,17 @@ package sp.kx.math
 
 /**
  * Stores two values. Can be used to store the coordinates of a point in 2D space.
- * @property x The coordinate of the x-axis.
- * @property y The coordinate of the y-axis.
  * @author [Stanley Wintergreen](https://github.com/kepocnhh)
  * @since 0.2.0
  */
 interface Point {
+    /**
+     * The coordinate of the x-axis.
+     */
     val x: Double
+
+    /**
+     * The coordinate of the y-axis.
+     */
     val y: Double
 }
