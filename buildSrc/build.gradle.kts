0bcfd482d757cb9e280ec9b4a3653865d5a1737a
Stanley Wintergreen <stanley@Stanleys-MacBook-Air.local>
2023-06-02 19:26:08 +0300

    fix build src

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,15 +1,12 @@
-repositories.mavenCentral()
+repositories {
+    mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
 
 plugins {
-    id("org.gradle.kotlin.kotlin-dsl") version "2.1.7"
+    `kotlin-dsl`
 }
 
-val jvmTarget = "1.8"
-
-tasks.getByName<JavaCompile>("compileJava") {
-    targetCompatibility = jvmTarget
-}
-
-tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
-    kotlinOptions.jvmTarget = jvmTarget
+dependencies {
+    implementation("com.github.kepocnhh:GradleExtension.Core:0.3.0-SNAPSHOT")
 }
