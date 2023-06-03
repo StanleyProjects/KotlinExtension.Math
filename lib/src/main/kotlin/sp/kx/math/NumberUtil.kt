af1bb16a27751216d74109bea0e2a50f857a2d30
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-03 17:25:54 +0300

    fix doc

--- lib/src/main/kotlin/sp/kx/math/NumberUtil.kt
+++ lib/src/main/kotlin/sp/kx/math/NumberUtil.kt
@@ -3,6 +3,19 @@ package sp.kx.math
 import sp.kx.math.unsafe.toString
 import java.util.Locale
 
+/**
+ * Usage:
+ * ```
+ * assertEquals("1.23", 1.234567.toString(points = 2))
+ * ```
+ * @return A [String] of a formatted [Double] number.
+ * @receiver The value of this number will be formatted.
+ * @param points The number of decimal places to be displayed in the summary [String].
+ * @param locale To apply during formatting. Default is [Locale.US].
+ * @throws IllegalStateException if [points] count is negative.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.2.1
+ */
 fun Double.toString(points: Int, locale: Locale = Locale.US): String {
     if (points < 0) error("Points count is negative!")
     return toString(number = this, points, locale)
--- lib/src/main/kotlin/sp/kx/math/PointUtil.kt
+++ lib/src/main/kotlin/sp/kx/math/PointUtil.kt
@@ -3,6 +3,19 @@ package sp.kx.math
 import sp.kx.math.unsafe.toString
 import java.util.Locale
 
+/**
+ * Usage:
+ * ```
+ * assertEquals("{x: 1.20, y: 3.40}", pointOf(x = 1.2, y = 3.4).toString(points = 2))
+ * ```
+ * @return Coordinates of the receiver [Point] in formatted form.
+ * @receiver The coordinates of this [Point] will be displayed in the summary [String].
+ * @param points The number of decimal places that each coordinate will have.
+ * @param locale To apply during formatting. Default is [Locale.US].
+ * @throws IllegalStateException if [points] count is negative.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.2.1
+ */
 fun Point.toString(points: Int, locale: Locale = Locale.US): String {
     if (points < 0) error("Points count is negative!")
     return toString(point = this, points, locale)
