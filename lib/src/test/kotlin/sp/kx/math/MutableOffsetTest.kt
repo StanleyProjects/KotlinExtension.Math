c05babb03d4e1b6b4f8c943ef662a7d4857116ef
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-04 20:02:26 +0300

    test mutable offset

--- lib/src/test/kotlin/sp/kx/math/MutableOffsetTest.kt
+++ lib/src/test/kotlin/sp/kx/math/MutableOffsetTest.kt
@@ -20,4 +20,86 @@ internal class MutableOffsetTest {
         Assertions.assertEquals(5.6, actual.dX)
         Assertions.assertEquals(7.8, actual.dY)
     }
+
+    @Test
+    fun toStringTest() {
+        MutableOffset(dX = 1.2, dY = 5.6).also { actual ->
+            Assertions.assertEquals("{dX: 1.20, dY: 5.60}", actual.toString())
+        }
+        MutableOffset(dX = 1.23, dY = 5.67).also { actual ->
+            Assertions.assertEquals("{dX: 1.23, dY: 5.67}", actual.toString())
+        }
+        MutableOffset(dX = 1.234, dY = 5.678).also { actual ->
+            Assertions.assertEquals("{dX: 1.23, dY: 5.68}", actual.toString())
+        }
+    }
+
+    @Test
+    fun equalsTest() {
+        val foo = MutableOffset(dX = 1.2, dY = 3.4)
+        val bar = MutableOffset(dX = 1.2, dY = 3.4)
+        Assertions.assertEquals(foo, bar)
+        Assertions.assertFalse(foo === bar)
+        Assertions.assertTrue(foo == bar)
+    }
+
+    @Test
+    fun equalsNotTest() {
+        val actual = MutableOffset(dX = 1.2, dY = 3.4)
+        MutableOffset(dX = 1.2, dY = 1.2).also { unexpected ->
+            Assertions.assertFalse(unexpected === actual)
+            Assertions.assertNotEquals(unexpected, actual)
+            Assertions.assertFalse(unexpected == actual)
+        }
+        MutableOffset(dX = 3.4, dY = 3.4).also { unexpected ->
+            Assertions.assertFalse(unexpected === actual)
+            Assertions.assertNotEquals(unexpected, actual)
+            Assertions.assertFalse(unexpected == actual)
+        }
+        MutableOffset(dX = 3.4, dY = 1.2).also { unexpected ->
+            Assertions.assertFalse(unexpected === actual)
+            Assertions.assertNotEquals(unexpected, actual)
+            Assertions.assertFalse(unexpected == actual)
+        }
+        Assertions.assertFalse(actual.equals(Unit))
+    }
+
+    @Test
+    fun hashCodeTest() {
+        MutableOffset(dX = 1.2, dY = 3.4).hashCode()
+    }
+
+    @Test
+    fun setTest() {
+        val actual = MutableOffset(dX = 1.2, dY = 3.4)
+        Assertions.assertNotEquals(actual.dX, actual.dY)
+        Assertions.assertEquals(1.2, actual.dX)
+        Assertions.assertEquals(3.4, actual.dY)
+        actual.set(dX = 5.6, dY = 7.8)
+        Assertions.assertEquals(5.6, actual.dX)
+        Assertions.assertEquals(7.8, actual.dY)
+    }
+
+    @Test
+    fun setOffsetTest() {
+        val foo = MutableOffset(dX = 1.2, dY = 3.4)
+        Assertions.assertNotEquals(foo.dX, foo.dY)
+        Assertions.assertEquals(1.2, foo.dX)
+        Assertions.assertEquals(3.4, foo.dY)
+        val bar = offsetOf(dX = 5.6, dY = 7.8)
+        Assertions.assertNotEquals(foo, bar)
+        foo.set(bar)
+        Assertions.assertEquals(foo, bar)
+    }
+
+    @Test
+    fun swapTest() {
+        val actual = MutableOffset(dX = 1.2, dY = 3.4)
+        Assertions.assertNotEquals(actual.dX, actual.dY)
+        Assertions.assertEquals(1.2, actual.dX)
+        Assertions.assertEquals(3.4, actual.dY)
+        actual.swap()
+        Assertions.assertEquals(3.4, actual.dX)
+        Assertions.assertEquals(1.2, actual.dY)
+    }
 }
