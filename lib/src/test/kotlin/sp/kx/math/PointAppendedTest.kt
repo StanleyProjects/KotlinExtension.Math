8c897bd338fc977882e2278ab3e3bb388fbbe871
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-06 20:42:32 +0300

    test point appended

---
+++ lib/src/test/kotlin/sp/kx/math/PointAppendedTest.kt
@@ -0,0 +1,66 @@
+package sp.kx.math
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class PointAppendedTest {
+    @Test
+    fun appendedTest() {
+        val foo = pointOf(x = 1.2, y = 3.4)
+        Assertions.assertNotEquals(foo.x, foo.y)
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        (5.6 to 7.8).also { (dX, dY) ->
+            Assertions.assertNotEquals(dX, dY)
+            val bar = foo.appended(dX = dX, dY = dY)
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(bar.x, bar.y)
+            Assertions.assertEquals(1.2 + dX, bar.x)
+            Assertions.assertEquals(3.4 + dY, bar.y)
+            Assertions.assertEquals(foo.x + dX, bar.x)
+            Assertions.assertEquals(foo.y + dY, bar.y)
+        }
+        (-1.28 to -2.56).also { (dX, dY) ->
+            Assertions.assertNotEquals(dX, dY)
+            val bar = foo.appended(dX = dX, dY = dY)
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(bar.x, bar.y)
+            Assertions.assertEquals(1.2 + dX, bar.x)
+            Assertions.assertEquals(3.4 + dY, bar.y)
+            Assertions.assertEquals(foo.x + dX, bar.x)
+            Assertions.assertEquals(foo.y + dY, bar.y)
+        }
+    }
+
+    @Test
+    fun appendedOffsetTest() {
+        val foo = pointOf(x = 1.2, y = 3.4)
+        Assertions.assertNotEquals(foo.x, foo.y)
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        offsetOf(dX = 5.6, dY = 7.8).also { offset ->
+            Assertions.assertNotEquals(offset.dX, offset.dY)
+            val bar = foo.appended(offset = offset)
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(bar.x, bar.y)
+            Assertions.assertEquals(1.2 + offset.dX, bar.x)
+            Assertions.assertEquals(3.4 + offset.dY, bar.y)
+            Assertions.assertEquals(foo.x + offset.dX, bar.x)
+            Assertions.assertEquals(foo.y + offset.dY, bar.y)
+        }
+        offsetOf(dX = -1.28, dY = -2.56).also { offset ->
+            Assertions.assertNotEquals(offset.dX, offset.dY)
+            val bar = foo.appended(offset = offset)
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(bar.x, bar.y)
+            Assertions.assertEquals(1.2 + offset.dX, bar.x)
+            Assertions.assertEquals(3.4 + offset.dY, bar.y)
+            Assertions.assertEquals(foo.x + offset.dX, bar.x)
+            Assertions.assertEquals(foo.y + offset.dY, bar.y)
+        }
+    }
+}
