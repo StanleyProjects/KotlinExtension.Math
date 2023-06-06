2844b5833a1f5d9c3383e36d92b08d7b3181892c
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-06 16:03:06 +0300

    test point move

--- lib/src/test/kotlin/sp/kx/math/MutablePointTest.kt
+++ lib/src/test/kotlin/sp/kx/math/MutablePointTest.kt
@@ -193,10 +193,46 @@ internal class MutablePointTest {
             Assertions.assertEquals(1.2, point.x)
             Assertions.assertEquals(3.4, point.y)
             val length = 5.6
+            val angle = kotlin.math.PI / 4
+            point.move(length = length, angle = angle)
+            Assertions.assertEquals(1.2 + length * kotlin.math.cos(angle), point.x)
+            Assertions.assertEquals(3.4 + length * kotlin.math.sin(angle), point.y)
+        }
+        MutablePoint(x = 1.2, y = 3.4).also { point ->
+            Assertions.assertNotEquals(point.x, point.y)
+            Assertions.assertEquals(1.2, point.x)
+            Assertions.assertEquals(3.4, point.y)
+            val length = 5.6
             val angle = kotlin.math.PI / 2
             point.move(length = length, angle = angle)
             Assertions.assertEquals(1.2, point.x, 0.000000000000001)
+            Assertions.assertNotEquals(1.2, point.x, 0.0000000000000001)
+            Assertions.assertTrue(point.x.eq(1.2, points = 15))
+            Assertions.assertFalse(point.x.eq(1.2, points = 16))
             Assertions.assertEquals(3.4 + length, point.y)
+        }
+        MutablePoint(x = 1.2, y = 3.4).also { point ->
+            Assertions.assertNotEquals(point.x, point.y)
+            Assertions.assertEquals(1.2, point.x)
+            Assertions.assertEquals(3.4, point.y)
+            val length = 5.6
+            val angle = kotlin.math.PI / 2 + kotlin.math.PI / 4
+            point.move(length = length, angle = angle)
+            Assertions.assertEquals(1.2 + length * kotlin.math.cos(angle), point.x)
+            Assertions.assertEquals(3.4 + length * kotlin.math.sin(angle), point.y)
+        }
+        MutablePoint(x = 1.2, y = 3.4).also { point ->
+            Assertions.assertNotEquals(point.x, point.y)
+            Assertions.assertEquals(1.2, point.x)
+            Assertions.assertEquals(3.4, point.y)
+            val length = 5.6
+            val angle = kotlin.math.PI
+            point.move(length = length, angle = angle)
+            Assertions.assertEquals(1.2 - length, point.x)
+            Assertions.assertEquals(3.4, point.y, 0.000000000000001)
+            Assertions.assertNotEquals(3.4, point.y, 0.0000000000000001)
+            Assertions.assertTrue(point.y.eq(3.4, points = 15))
+            Assertions.assertFalse(point.y.eq(3.4, points = 16))
         }
     }
 }
