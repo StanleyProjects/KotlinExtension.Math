542a872746cc5b53e2ea8fef546aa046b6a3a994
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-06 21:02:26 +0300

    test point offset

--- lib/src/test/kotlin/sp/kx/math/PointUtilTest.kt
+++ lib/src/test/kotlin/sp/kx/math/PointUtilTest.kt
@@ -99,4 +99,62 @@ internal class PointUtilTest {
             Assertions.assertEquals(bar.y, foo.x)
         }
     }
+
+    @Test
+    fun offsetTest() {
+        val foo = pointOf(x = 1.2, y = 3.4)
+        Assertions.assertNotEquals(foo.x, foo.y)
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        foo.offset(x = 5.6, y = 8.9).also { offset ->
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(offset.dX, offset.dY)
+            Assertions.assertEquals(foo.x - 5.6, offset.dX)
+            Assertions.assertEquals(foo.y - 8.9, offset.dY)
+            Assertions.assertEquals(-4.4, offset.dX, 0.000001)
+            Assertions.assertTrue((-4.4).eq(offset.dX, points = 6))
+            Assertions.assertEquals(-5.5, offset.dY)
+        }
+        foo.offset(x = -1.28, y = -2.56).also { offset ->
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(offset.dX, offset.dY)
+            Assertions.assertEquals(foo.x + 1.28, offset.dX)
+            Assertions.assertEquals(foo.y + 2.56, offset.dY)
+            Assertions.assertEquals(2.48, offset.dX)
+            Assertions.assertEquals(5.96, offset.dY)
+        }
+    }
+
+    @Test
+    fun offsetPointTest() {
+        val foo = pointOf(x = 1.2, y = 3.4)
+        Assertions.assertNotEquals(foo.x, foo.y)
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        pointOf(x = 5.6, y = 8.9).also { bar ->
+            val offset = foo.offset(other = bar)
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(bar.x, bar.y)
+            Assertions.assertNotEquals(offset.dX, offset.dY)
+            Assertions.assertEquals(foo.x - bar.x, offset.dX)
+            Assertions.assertEquals(foo.y - bar.y, offset.dY)
+            Assertions.assertEquals(-4.4, offset.dX, 0.000001)
+            Assertions.assertTrue((-4.4).eq(offset.dX, points = 6))
+            Assertions.assertEquals(-5.5, offset.dY)
+        }
+        pointOf(x = -1.28, y = -2.56).also { bar ->
+            val offset = foo.offset(other = bar)
+            Assertions.assertEquals(1.2, foo.x)
+            Assertions.assertEquals(3.4, foo.y)
+            Assertions.assertNotEquals(bar.x, bar.y)
+            Assertions.assertNotEquals(offset.dX, offset.dY)
+            Assertions.assertEquals(foo.x - bar.x, offset.dX)
+            Assertions.assertEquals(foo.y - bar.y, offset.dY)
+            Assertions.assertEquals(2.48, offset.dX)
+            Assertions.assertEquals(5.96, offset.dY)
+        }
+    }
 }
