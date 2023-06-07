c18c62482a3c698811676f102a779ad8108d3454
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-07 18:00:19 +0300

    test mutable vector equals

--- lib/src/test/kotlin/sp/kx/math/MutableVectorTest.kt
+++ lib/src/test/kotlin/sp/kx/math/MutableVectorTest.kt
@@ -59,4 +59,23 @@ internal class MutableVectorTest {
             Assertions.assertEquals("{x: 1.23, y: 3.46} -> {x: 5.68, y: 7.89}", actual.toString())
         }
     }
+
+    @Test
+    fun equalsTest() {
+        val foo = MutableVector(
+            start = MutablePoint(x = 1.2, y = 3.4),
+            finish = MutablePoint(x = 5.6, y = 7.8),
+        )
+        val bar = MutableVector(
+            start = MutablePoint(x = 1.2, y = 3.4),
+            finish = MutablePoint(x = 5.6, y = 7.8),
+        )
+        Assertions.assertEquals(foo, bar)
+        Assertions.assertFalse(foo === bar)
+        Assertions.assertTrue(foo == bar)
+        Assertions.assertFalse(foo.start === bar.start)
+        Assertions.assertTrue(foo.start == bar.start)
+        Assertions.assertFalse(foo.finish === bar.finish)
+        Assertions.assertTrue(foo.finish == bar.finish)
+    }
 }
