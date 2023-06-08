26da91c688e835efb9928e1cb8bbf3ba31d89616
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-08 17:32:04 +0300

    fix code style

--- lib/src/main/kotlin/sp/kx/math/unsafe/VectorUnsafe.kt
+++ lib/src/main/kotlin/sp/kx/math/unsafe/VectorUnsafe.kt
@@ -8,8 +8,6 @@ internal fun toString(vector: Vector, points: Int, locale: Locale): String {
 }
 
 internal fun eq(it: Vector, other: Vector, points: Int): Boolean {
-    return eq(it.start.x, other.start.x, points)
-            && eq(it.start.y, other.start.y, points)
-            && eq(it.finish.x, other.finish.x, points)
-            && eq(it.finish.y, other.finish.y, points)
+    return eq(it.start.x, other.start.x, points) && eq(it.start.y, other.start.y, points) &&
+        eq(it.finish.x, other.finish.x, points) && eq(it.finish.y, other.finish.y, points)
 }
