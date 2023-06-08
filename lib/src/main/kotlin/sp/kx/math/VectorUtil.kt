31016583cb27490178c01397269b56bf444d9120
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-08 17:27:58 +0300

    vector util

---
+++ lib/src/main/kotlin/sp/kx/math/VectorUtil.kt
@@ -0,0 +1,15 @@
+package sp.kx.math
+
+import sp.kx.math.unsafe.eq
+import sp.kx.math.unsafe.toString
+import java.util.Locale
+
+fun Vector.toString(points: Int, locale: Locale = Locale.US): String {
+    if (points < 0) error("Points count is negative!")
+    return toString(vector = this, points = points, locale = locale)
+}
+
+fun Vector.eq(other: Vector, points: Int): Boolean {
+    if (points < 0) error("Points count is negative!")
+    return eq(it = this, other = other, points = points)
+}
