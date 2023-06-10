a869dcf3e9899df63b1ef41287a8c762b526321d
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-10 21:21:49 +0300

    doc point util

--- lib/src/main/kotlin/sp/kx/math/PointCalculations.kt
+++ lib/src/main/kotlin/sp/kx/math/PointCalculations.kt
@@ -1,5 +1,24 @@
 package sp.kx.math
 
+/**
+ * Usage:
+ * ```
+ * val distance = distanceOf(a = pointOf(1, 1), b = pointOf(3, 1))
+ * assertEquals(2.0, distance)
+ *
+ *   ^
+ *   |
+ * 2 -
+ *   |    a       b
+ * 1 -   * - - - *
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return Distance between point [a] and point [b].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.2
+ */
 fun distanceOf(
     a: Point,
     b: Point,
@@ -12,6 +31,27 @@ fun distanceOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val angle = angleOf(a = pointOf(1, 1), b = pointOf(3, 3))
+ * assertEquals(PI / 4, angle)
+ *
+ *   ^
+ *   |
+ * 3 -           * b
+ *   |         /
+ * 2 -       /
+ *   |     /
+ * 1 -   * a
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return The angle in radians between the x-axis and the straight line containing point [a] and point [b].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.2
+ */
 fun angleOf(
     a: Point,
     b: Point,
@@ -24,6 +64,27 @@ fun angleOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val point = centerOf(aX = 1.0, aY = 1.0, bX = 3.0, bY = 1.0)
+ * assertEquals(2.0, point.x)
+ * assertEquals(1.0, point.y)
+ *
+ *   ^
+ *   |
+ * 2 -
+ *   |
+ * y -   * - x - *
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   aX  2   bX  4
+ * ```
+ * @return A [Point] that is the center of the segment
+ * between point `a` with [aX] and [aY] coordinates and point `b` with [bX] and [bY] coordinates.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.2
+ */
 @Suppress("MagicNumber")
 fun centerOf(
     aX: Double,
@@ -37,6 +98,26 @@ fun centerOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val point = centerOf(a = pointOf(1, 1), b = pointOf(3, 1))
+ * assertEquals(2.0, point.x)
+ * assertEquals(1.0, point.y)
+ *
+ *   ^
+ *   |
+ * 2 -
+ *   |    a       b
+ * 1 -   * - x - *
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return A [Point] that is the center of the segment between point [a] and point [b].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.2
+ */
 fun centerOf(
     a: Point,
     b: Point,
