5d4005d0fe357a39becc04f0f910bfd2a81cc09f
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-11 17:58:13 +0300

    empty vector

--- lib/src/main/kotlin/sp/kx/math/EmptyVector.kt
+++ lib/src/main/kotlin/sp/kx/math/EmptyVector.kt
@@ -1,6 +1,28 @@
 package sp.kx.math
 
+import sp.kx.math.unsafe.toString
+import java.util.Locale
+import java.util.Objects
+
 private class EmptyVector(point: Point) : Vector {
     override val start: Point = point
     override val finish: Point = point
+
+    override fun toString(): String {
+        return "EmptyVector${toString(point = start, points = 2, locale = Locale.US)}"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Vector) return false
+        return start.x == other.start.x &&
+                finish.x == other.finish.x && start.y == other.start.y && finish.y == other.finish.y
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(start.x, start.y, finish.x, finish.y)
+    }
+}
+
+fun Point.toVector(): Vector {
+    return EmptyVector(this)
 }
