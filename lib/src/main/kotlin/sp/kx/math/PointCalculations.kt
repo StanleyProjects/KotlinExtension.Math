659f1c37fadcb7a2fa4f317ebb2f13e58dada1c0
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-14 15:28:54 +0300

    doc point calculations

--- lib/src/main/kotlin/sp/kx/math/PointCalculations.kt
+++ lib/src/main/kotlin/sp/kx/math/PointCalculations.kt
@@ -64,6 +64,27 @@ fun angleOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val angle = angleOf(a = pointOf(1, 1), bX = 3.0, bY = 3.0)
+ * assertEquals(PI / 4, angle)
+ *
+ *   ^
+ *   |
+ * 3 -           * b
+ *   |         /
+ * 2 -       /
+ *   |     /
+ * 1 -   * a
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return The angle in radians between the x-axis and the straight line containing point [a] and point `b` with [bX] and [bY] coordinates.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.4
+ */
 fun angleOf(
     a: Point,
     bX: Double,
@@ -77,6 +98,27 @@ fun angleOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val angle = angleOf(aX = 1.0, aY = 1.0, b = pointOf(3, 3))
+ * assertEquals(PI / 4, angle)
+ *
+ *   ^
+ *   |
+ * 3 -           * b
+ *   |         /
+ * 2 -       /
+ *   |     /
+ * 1 -   * a
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return The angle in radians between the x-axis and the straight line containing point `a` with [aX] and [aY] coordinates and point [b].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.4
+ */
 fun angleOf(
     aX: Double,
     aY: Double,
@@ -90,6 +132,27 @@ fun angleOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val angle = angleOf(a = pointOf(1, 1), offset = offsetOf(2.0, 2.0))
+ * assertEquals(PI / 4, angle)
+ *
+ *   ^
+ *   |
+ * 3 -           * b
+ *   |         /
+ * 2 -       /
+ *   |     /
+ * 1 -   * a
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return The angle in radians between the x-axis and the straight line containing point [a] and point `b` with `a.x + offset.dX` and `a.y + offset.dY` coordinates.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.4
+ */
 fun angleOf(
     a: Point,
     offset: Offset,
@@ -100,6 +163,29 @@ fun angleOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val angle = angleOf(a = pointOf(0, 0), b = pointOf(2, 2), offset = offsetOf(1.0, 1.0))
+ * assertEquals(PI / 4, angle)
+ *
+ *   ^
+ *   |
+ * 3 -           * b
+ *   |         /
+ * 2 -       x
+ *   |     /
+ * 1 -   * a
+ *   |
+ * 0 x---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return The angle in radians between the x-axis and the straight line containing
+ * point `a` with `a.x + offset.dX` and `a.y + offset.dY` coordinates and
+ * point `b` with `b.x + offset.dX` and `b.y + offset.dY` coordinates.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.4
+ */
 fun angleOf(
     a: Point,
     b: Point,
