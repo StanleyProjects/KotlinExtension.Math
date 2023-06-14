20c9880bba98a5759cc659dd6de3ba7fb5709eb1
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-14 15:19:28 +0300

    doc number util

--- lib/src/main/kotlin/sp/kx/math/NumberUtil.kt
+++ lib/src/main/kotlin/sp/kx/math/NumberUtil.kt
@@ -5,6 +5,12 @@ import sp.kx.math.unsafe.toString
 import java.util.Locale
 
 /**
+ * Decryption:
+ * ```
+ * 16:   |<---points--->|
+ *     1.2300000000000000
+ * ```
+ *
  * Usage:
  * ```
  * assertEquals("1.23", 1.234567.toString(points = 2))
@@ -22,6 +28,28 @@ fun Double.toString(points: Int, locale: Locale = Locale.US): String {
     return toString(number = this, points = points, locale = locale)
 }
 
+/**
+ * Decryption:
+ * ```
+ * 16:       |<---points--->|
+ * 22: |<-------total------>|
+ *     00001.2300000000000000
+ * ```
+ *
+ * Usage:
+ * ```
+ * assertEquals("01.23", 1.234567.toString(total = 5, points = 2))
+ * ```
+ * @return A [String] of a formatted [Double] number.
+ * @receiver The value of this number will be formatted.
+ * @param total The number of total places to be displayed in the summary [String].
+ * @param points The number of decimal places to be displayed in the summary [String].
+ * @param locale To apply during formatting. Default is [Locale.US].
+ * @throws IllegalArgumentException if [total] count is not positive.
+ * @throws IllegalStateException if [points] count is negative.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.4
+ */
 fun Double.toString(total: Int, points: Int, locale: Locale = Locale.US): String {
     require(total > 0) { "Total count is not positive!" }
     if (points < 0) error("Points count is negative!")
@@ -49,6 +77,22 @@ fun Double.eq(other: Double, points: Int): Boolean {
     return eq(it = this, other = other, points = points)
 }
 
+/**
+ * Function for coterminal conversion of a [Double].
+ * For example, it can be used to convert any radian value to a value between `0.0` and `kotlin.math.PI * 2`.
+ *
+ * Usage:
+ * ```
+ * assertEquals(kotlin.math.PI / 4, (kotlin.math.PI * 2 + kotlin.math.PI / 4).ct())
+ * assertEquals(63.0, (-1.0).ct(k = 64.0))
+ * assertEquals(1.0, 129.0.ct(k = 128.0))
+ * ```
+ * @receiver This [Double] will be converted to a coterminal [Double].
+ * @param k The coefficient which is used in the coterminal transformation. Default is `kotlin.math.PI * 2`.
+ * @return The coterminal [Double] relative to [this] receiver given the [k] coefficient.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.4
+ */
 fun Double.ct(k: Double = kotlin.math.PI * 2): Double {
     return (this % k + k) % k
 }
