4f0f7a02123a2db7849649801b30b2613821ec6b
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-14 19:49:52 +0300

    mut measure

--- lib/src/main/kotlin/sp/kx/math/measure/MutableDoubleMeasure.kt
+++ lib/src/main/kotlin/sp/kx/math/measure/MutableDoubleMeasure.kt
@@ -1,6 +1,20 @@
 package sp.kx.math.measure
 
+import java.util.Objects
+
 class MutableDoubleMeasure(var magnitude: Double) : Measure<Double, Double> {
+    override fun toString(): String {
+        return super.toString()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(magnitude)
+    }
+
     override fun transform(units: Double): Double {
         return units * magnitude
     }
