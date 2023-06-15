573598a46ef7c5b590cbb5bd688a2c5aa7cd4915
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-15 19:21:14 +0300

    fix code quality

--- lib/src/main/kotlin/sp/kx/math/unsafe/DoubleDeviationUnsafe.kt
+++ lib/src/main/kotlin/sp/kx/math/unsafe/DoubleDeviationUnsafe.kt
@@ -4,5 +4,11 @@ import sp.kx.math.measure.Deviation
 import java.util.Locale
 
 internal fun toString(deviation: Deviation<Double>, points: Int, locale: Locale): String {
-    return "{a: ${toString(number = deviation.actual, points, locale)}, e: ${toString(number = deviation.expected, points, locale)}}"
+    return StringBuilder("{")
+        .append("a: ")
+        .append(toString(number = deviation.actual, points, locale))
+        .append("e: ")
+        .append(toString(number = deviation.expected, points, locale))
+        .append("}")
+        .toString()
 }
