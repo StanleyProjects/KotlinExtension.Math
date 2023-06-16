f33bbe4e0398b72ffd74c3f04974fa41fbbf7994
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-16 17:14:23 +0300

    doc mut deviation

--- lib/src/main/kotlin/sp/kx/math/measure/MutableDeviation.kt
+++ lib/src/main/kotlin/sp/kx/math/measure/MutableDeviation.kt
@@ -1,5 +1,19 @@
 package sp.kx.math.measure
 
+/**
+ * A mutable implementation of the [Deviation] type.
+ *
+ * Usage:
+ * ```
+ * val deviation = MutableDeviation(actual = 3.0, expected = 2.0)
+ * deviation.actual = 2.0
+ * deviation.expected = 3.0
+ * ```
+ * @property actual Actual value.
+ * @property expected Expected value.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 class MutableDeviation<T : Comparable<T>>(
     override var actual: T,
     override var expected: T,
