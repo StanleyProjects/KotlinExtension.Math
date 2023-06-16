d52e9c34ecfa362d046b5008fb48383127ff624e
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-16 16:59:29 +0300

    doc mut size

--- lib/src/main/kotlin/sp/kx/math/MutableSize.kt
+++ lib/src/main/kotlin/sp/kx/math/MutableSize.kt
@@ -4,6 +4,20 @@ import sp.kx.math.unsafe.toString
 import java.util.Locale
 import java.util.Objects
 
+/**
+ * A mutable implementation of the [Size] type.
+ *
+ * Usage:
+ * ```
+ * val size = MutableSize(width = 3.0, height = 2.0)
+ * size.dX = 2.0
+ * size.dY = 3.0
+ * ```
+ * @property width Width size. Horizontal size. Parallel to the x-axis.
+ * @property height Height size. Vertical size. Parallel to the y-axis.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 class MutableSize(
     override var width: Double,
     override var height: Double,
@@ -22,6 +36,28 @@ class MutableSize(
     }
 }
 
+/**
+ * Usage:
+ * ```
+ * val size = sizeOf(width = 2.0, height = 1.0)
+ * val foo = pointOf(1, 1)
+ * val bar = pointOf(x = foo.x + size.width, y = foo.y + size.height)
+ *
+ *   ^
+ *   |
+ * 3 -
+ *   |
+ * 2 -            * bar
+ *   |
+ * 1 -   * foo
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return An instance of [Size] built from the [Double] values [width] and [height].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 fun sizeOf(
     width: Double,
     height: Double,
