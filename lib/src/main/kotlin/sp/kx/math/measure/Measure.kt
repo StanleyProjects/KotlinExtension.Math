be8b65cf18733dcb3840274ea4b55d0d22700752
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-16 17:10:11 +0300

    doc measure

--- lib/src/main/kotlin/sp/kx/math/measure/Measure.kt
+++ lib/src/main/kotlin/sp/kx/math/measure/Measure.kt
@@ -1,5 +1,17 @@
 package sp.kx.math.measure
 
+/**
+ * A type for convenient conversion of values with respect to the measurement system.
+ *
+ * Usage:
+ * ```
+ * val measure: Measure<Double, Double> = ...
+ * assertEquals(2.0, measure.magnitude)
+ * assertEquals(6.0, measure.transform(3.0))
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 interface Measure<T : Comparable<T>, U : Comparable<U>> {
     val magnitude: U
     fun transform(units: T): U
