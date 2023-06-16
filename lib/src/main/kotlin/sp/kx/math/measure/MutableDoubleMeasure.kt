639337bc35c8e2c268e8510a6e9ec599b049d59d
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-16 17:24:15 +0300

    doc mut measure

--- lib/src/main/kotlin/sp/kx/math/measure/MutableDoubleMeasure.kt
+++ lib/src/main/kotlin/sp/kx/math/measure/MutableDoubleMeasure.kt
@@ -4,6 +4,18 @@ import sp.kx.math.unsafe.toString
 import java.util.Locale
 import java.util.Objects
 
+/**
+ * A mutable implementation of the [Measure] type with [Double]s.
+ *
+ * Usage:
+ * ```
+ * val measure = MutableDoubleMeasure(magnitude = 2.0)
+ * measure.magnitude = 3.0
+ * ```
+ * @property magnitude Will be used in the transformation.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 class MutableDoubleMeasure(override var magnitude: Double) : Measure<Double, Double> {
     override fun toString(): String {
         return toString(measure = this, points = 2, locale = Locale.US)
