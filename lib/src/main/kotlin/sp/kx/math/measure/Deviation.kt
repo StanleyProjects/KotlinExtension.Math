daad66ea50fc894146aac8d3f8ee6b2e202a91b1
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-16 17:03:00 +0300

    doc deviation

--- lib/src/main/kotlin/sp/kx/math/measure/Deviation.kt
+++ lib/src/main/kotlin/sp/kx/math/measure/Deviation.kt
@@ -1,6 +1,26 @@
 package sp.kx.math.measure
 
+/**
+ * Stores two comparable values. Convenient for calculations. It is possible to keep the current value close to the expected one.
+ *
+ * Usage:
+ * ```
+ * val deviation: Deviation<Double> = ...
+ * if (deviation.actual != deviation.expected) {
+ *     println("Value is not expected!")
+ * }
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 interface Deviation<T : Comparable<T>> {
+    /**
+     * Actual value.
+     */
     val actual: T
+
+    /**
+     * Expected value.
+     */
     val expected: T
 }
