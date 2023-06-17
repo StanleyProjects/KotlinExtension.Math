3ca0e6e269978b69d3a6b62d5b937f78aa9133ab
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-17 16:31:40 +0300

    doc interval util

--- lib/src/main/kotlin/sp/kx/math/measure/IntervalUtil.kt
+++ lib/src/main/kotlin/sp/kx/math/measure/IntervalUtil.kt
@@ -1,5 +1,15 @@
 package sp.kx.math.measure
 
+/**
+ * Usage:
+ * ```
+ * assertTrue(intervalOf(a = 1.minutes, b = 1.minutes))
+ * assertFalse(intervalOf(a = 1.minutes, b = 3.minutes))
+ * ```
+ * @return `true` if the extreme [Interval.a] and [Interval.b] values of the durations are equal. `false` otherwise.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.5.0
+ */
 fun <T : Comparable<T>> Interval<T>.isEmpty(): Boolean {
     return a == b
 }
