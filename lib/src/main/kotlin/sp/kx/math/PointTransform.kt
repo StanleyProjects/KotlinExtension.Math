7446bba0812f812b26d543ef7d01714d2ca6e303
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-29 20:06:02 +0300

    test point transform

--- lib/src/main/kotlin/sp/kx/math/PointTransform.kt
+++ lib/src/main/kotlin/sp/kx/math/PointTransform.kt
@@ -1,5 +1,7 @@
 package sp.kx.math
 
+import sp.kx.math.measure.Measure
+
 fun pointOf(
     x: Double,
     y: Double,
@@ -17,5 +19,14 @@ fun Point.map(
     return pointOf(
         x = transform(x),
         y = transform(y),
+    )
+}
+
+fun Point.map(
+    measure: Measure<Double, Double>,
+): Point {
+    return pointOf(
+        x = measure.transform(x),
+        y = measure.transform(y),
     )
 }
---
+++ lib/src/test/kotlin/sp/kx/math/PointTransformTest.kt
@@ -0,0 +1,46 @@
+package sp.kx.math
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import sp.kx.math.measure.measureOf
+
+internal class PointTransformTest {
+    @Test
+    fun pointOfTest() {
+        val actual = pointOf(
+            x = 1.2,
+            y = 3.4,
+        ) {
+            it * 2
+        }
+        Assertions.assertNotEquals(actual.x, actual.y)
+        Assertions.assertEquals(1.2 * 2, actual.x)
+        Assertions.assertEquals(3.4 * 2, actual.y)
+    }
+
+    @Test
+    fun mapTest() {
+        val foo = pointOf(x = 1.2, y = 3.4)
+        Assertions.assertNotEquals(foo.x, foo.y)
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        val bar = foo.map { it * 2 }
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        Assertions.assertEquals(1.2 * 2, bar.x)
+        Assertions.assertEquals(3.4 * 2, bar.y)
+    }
+
+    @Test
+    fun mapMeasureTest() {
+        val foo = pointOf(x = 1.2, y = 3.4)
+        Assertions.assertNotEquals(foo.x, foo.y)
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        val bar = foo.map(measureOf(magnitude = 2.0))
+        Assertions.assertEquals(1.2, foo.x)
+        Assertions.assertEquals(3.4, foo.y)
+        Assertions.assertEquals(1.2 * 2, bar.x)
+        Assertions.assertEquals(3.4 * 2, bar.y)
+    }
+}
