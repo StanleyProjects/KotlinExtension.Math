71fb8fd46d03262cd6319e11198ab4bd9de79975
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-29 20:15:23 +0300

    test vector offset

--- lib/src/test/kotlin/sp/kx/math/VectorConstructorTest.kt
+++ lib/src/test/kotlin/sp/kx/math/VectorConstructorTest.kt
@@ -60,44 +60,6 @@ internal class VectorConstructorTest {
     }
 
     @Test
-    fun toVectorOffsetTest() {
-        val foo = pointOf(x = 1.2, y = 3.4)
-        val offset = offsetOf(dX = 12.8, dY = 25.6)
-        val actual: Vector = foo.toVector(offset = offset)
-        Assertions.assertEquals(actual.start, foo)
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2, actual.start.x)
-        Assertions.assertEquals(3.4, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(1.2 + 12.8, actual.finish.x)
-        Assertions.assertEquals(3.4 + 25.6, actual.finish.y)
-        Assertions.assertEquals(1.2 + offset.dX, actual.finish.x)
-        Assertions.assertEquals(3.4 + offset.dY, actual.finish.y)
-        Assertions.assertEquals(actual.start.x + offset.dX, actual.finish.x)
-        Assertions.assertEquals(actual.start.y + offset.dY, actual.finish.y)
-    }
-
-    @Test
-    fun toVectorPointOffsetTest() {
-        val offset = offsetOf(dX = 12.8, dY = 25.6)
-        val foo = pointOf(x = 1.2, y = 3.4)
-        val bar = pointOf(x = 5.6, y = 7.8)
-        val actual: Vector = foo.toVector(finish = bar, offset = offset)
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2 + offset.dX, actual.start.x)
-        Assertions.assertEquals(3.4 + offset.dY, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(5.6 + offset.dX, actual.finish.x)
-        Assertions.assertEquals(7.8 + offset.dY, actual.finish.y)
-        Assertions.assertEquals(foo.x + offset.dX, actual.start.x)
-        Assertions.assertEquals(foo.y + offset.dY, actual.start.y)
-        Assertions.assertEquals(bar.x + offset.dX, actual.finish.x)
-        Assertions.assertEquals(bar.y + offset.dY, actual.finish.y)
-    }
-
-    @Test
     fun vectorOfLengthAngleTest() {
         val foo = pointOf(x = 1.2, y = 3.4)
         Assertions.assertNotEquals(foo.x, foo.y)
