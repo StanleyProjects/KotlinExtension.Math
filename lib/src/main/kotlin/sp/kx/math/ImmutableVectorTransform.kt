b3952a137de089619730e96de46f46b6a7c5c75b
Stanley Wintergreen <kepocnhh@gmail.com>
2023-08-25 17:08:31 +0300

    doc vector transform

--- lib/src/main/kotlin/sp/kx/math/ImmutableVectorTransform.kt
+++ lib/src/main/kotlin/sp/kx/math/ImmutableVectorTransform.kt
@@ -1,5 +1,32 @@
 package sp.kx.math
 
+/**
+ * Usage:
+ * ```
+ * val vector = vectorOf(
+ *     startX = 1.0,
+ *     startY = 2.0,
+ *     finishX = 2.0,
+ *     finishY = 1.0,
+ * ) { it * 2 }
+ *
+ *   ^
+ *   |
+ * 4 -   -   * vector.start
+ *   |
+ * 3 -       |
+ *   |
+ * 2 -   -   -   -   * vector.finish
+ *   |
+ * 1 -       |       |
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   x   4
+ * ```
+ * @return An instance of [Vector] built from the [Double] values and the [transform] function.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 fun vectorOf(
     startX: Double,
     startY: Double,
@@ -13,6 +40,32 @@ fun vectorOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val vector = vectorOf(
+ *     startX = 1.0,
+ *     startY = 2.0,
+ *     finish = pointOf(x = 2.0, y = 1.0),
+ * ) { it * 2 }
+ *
+ *   ^
+ *   |
+ * 4 -   -   * vector.start
+ *   |
+ * 3 -       |
+ *   |
+ * 2 -   -   -   -   * vector.finish
+ *   |
+ * 1 -       |       |
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   x   4
+ * ```
+ * @return An instance of [Vector] built from the [Double] values, [Point] and the [transform] function.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 fun vectorOf(
     startX: Double,
     startY: Double,
@@ -25,6 +78,32 @@ fun vectorOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = pointOf(x = 1.0, y = 2.0)
+ * val vector = foo.toVector(x = 2.0, y = 1.0) { it * 2 }
+ *
+ *   ^
+ *   |
+ * 4 -   -   * vector.start
+ *   |
+ * 3 -       |
+ *   |
+ * 2 -   -   -   -   * vector.finish
+ *   |
+ * 1 -       |       |
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   x   4
+ * ```
+ * @receiver This [Point] will become the [Vector.start] point transformed by the [transform] function.
+ * @param x This value will become the x-coordinate of the [Vector.finish] point transformed by the [transform] function.
+ * @param y This value will become the y-coordinate of the [Vector.finish] point transformed by the [transform] function.
+ * @return An instance of [Vector] built from the [Double] values, [Point] and the [transform] function.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 fun Point.toVector(
     x: Double,
     y: Double,
@@ -36,6 +115,29 @@ fun Point.toVector(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = pointOf(x = 1.0, y = 2.0) + pointOf(x = 2.0, y = 1.0)
+ * val bar = foo.map { it * 2 }
+ *
+ *   ^
+ *   |
+ * 4 -       * bar.start
+ *   |
+ * 3 -
+ *   |
+ * 2 -   * foo.start * bar.finish
+ *   |
+ * 1 -       * foo.finish
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   x   4
+ * ```
+ * @return An instance of [Vector] built from [this] receiver [Vector] and the [transform] function.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 fun Vector.map(
     transform: (Double) -> Double,
 ): Vector {
