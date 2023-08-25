473a2f66324078efc87b91dbafc20228f1c388bd
Stanley Wintergreen <kepocnhh@gmail.com>
2023-08-25 18:12:32 +0300

    doc point transform

--- lib/src/main/kotlin/sp/kx/math/PointTransform.kt
+++ lib/src/main/kotlin/sp/kx/math/PointTransform.kt
@@ -1,5 +1,27 @@
 package sp.kx.math
 
+/**
+ * Usage:
+ * ```
+ * val point = pointOf(x = 1.0, y = 2.0) { it * 2 }
+ *
+ *   ^
+ *   |
+ * 4 -   -   * point
+ *   |
+ * 3 -       |
+ *   |
+ * 2 -       |
+ *   |
+ * 1 -       |
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return An instance of [Point] built from the [Double] values [x], [y] and the [transform] function.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 fun pointOf(
     x: Double,
     y: Double,
@@ -11,6 +33,29 @@ fun pointOf(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = pointOf(x = 1.0, y = 2.0)
+ * val bar = foo.map { it * 2 }
+ *
+ *   ^
+ *   |
+ * 4 -   -   * bar
+ *   |
+ * 3 -       |
+ *   |
+ * 2 -   * foo
+ *   |
+ * 1 -   |   |
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return An instance of [Point] built from [this] receiver [Point] and the [transform] function.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.0
+ */
 fun Point.map(
     transform: (Double) -> Double,
 ): Point {
