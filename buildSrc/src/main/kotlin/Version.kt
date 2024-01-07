da4de84babe37c58d16be1ace469c41b374c6bce
Stanley Wintergreen <kepocnhh@gmail.com>
2024-01-07 19:03:21 +0300

    jupiter:5.10.1

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,7 +2,7 @@ object Version {
     const val detekt = "1.22.0"
     const val dokka = "1.8.10"
     const val jacoco = "0.8.9"
-    const val jupiter = "5.9.3"
+    const val jupiter = "5.10.1"
     const val jvmTarget = "17"
     const val kotlin = "1.9.20"
     const val ktlint = "0.49.1"
