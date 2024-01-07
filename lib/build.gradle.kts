c4a1aa8137291906a8c2e36237406d722fe219e4
Stanley Wintergreen <kepocnhh@gmail.com>
2024-01-07 19:02:35 +0300

    fix unit test task

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -64,20 +64,29 @@ val taskUnitTest = task<Test>("checkUnitTest") {
     useJUnitPlatform()
     testClassesDirs = sourceSets.test.get().output.classesDirs
     classpath = sourceSets.test.get().runtimeClasspath
+    val executionData = layout.buildDirectory.get()
+        .dir("jacoco")
+        .file("$name.exec")
+    doLast {
+        executionData.existing().file().filled()
+    }
 }
 
 val taskCoverageReport = task<JacocoReport>("assembleCoverageReport") {
     dependsOn(taskUnitTest)
     reports {
-        csv.required.set(false)
-        html.required.set(true)
-        xml.required.set(false)
+        html.required = true
+        csv.required = false
+        xml.required = false
     }
     sourceDirectories.setFrom(file("src/main/kotlin"))
     classDirectories.setFrom(sourceSets.main.get().output.classesDirs)
     executionData(taskUnitTest)
     doLast {
-        val report = buildDir.resolve("reports/jacoco/$name/html/index.html")
+        val report = layout.buildDirectory.get()
+            .dir("reports/jacoco/$name/html")
+            .file("index.html")
+            .asFile
         if (report.exists()) {
             println("Coverage report: ${report.absolutePath}")
         }
