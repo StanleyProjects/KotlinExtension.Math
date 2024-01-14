e6b59e9ec17e0e64b2d5435232b9a10a456ab4d6
Stanley Wintergreen <kepocnhh@gmail.com>
2024-01-14 18:22:48 +0300

    point calculations perpendicular unit test

--- lib/src/test/kotlin/sp/kx/math/PointCalculationsPerpendicularTest.kt
+++ lib/src/test/kotlin/sp/kx/math/PointCalculationsPerpendicularTest.kt
@@ -5,17 +5,89 @@ import org.junit.jupiter.api.Test
 
 internal class PointCalculationsPerpendicularTest {
     @Test
-    fun getPerpendicularTest() {
-        val a = pointOf(x = 2, y = 4)
-        val b = pointOf(x = 1, y = 1)
-        val c = pointOf(x = 5, y = 3)
-        val p = getPerpendicular(
-            a = a,
-            b = b,
-            c = c,
+    fun getPerpendicularZeroCTest() {
+        val targets = listOf(
+            pointOf(x = -5, y = 0) to pointOf(x = -4, y = 2),
+            pointOf(x = -3, y = -1) to pointOf(x = -2, y = 1),
+            pointOf(x = -3, y = 4) to pointOf(x = -4, y = 2),
+            pointOf(x = -1, y = 3) to pointOf(x = -2, y = 1),
+            pointOf(x = 1, y = 2) to pointOf(x = 0, y = 0),
+            pointOf(x = 3, y = 1) to pointOf(x = 2, y = -1),
+            pointOf(x = 5, y = 0) to pointOf(x = 4, y = -2),
+            pointOf(x = -1, y = -2) to pointOf(x = 0, y = 0),
+            pointOf(x = 1, y = -3) to pointOf(x = 2, y = -1),
+            pointOf(x = 3, y = -4) to pointOf(x = 4, y = -2),
         )
-        Assertions.assertEquals(p.x, 3.0)
-        Assertions.assertEquals(p.y, 2.0)
+        check(targets.size == 10)
+        check(targets.toSet().size == targets.size)
+        val b = pointOf(x = -4, y = 2)
+        val c = pointOf(x = 0, y = 0)
+        check(c.x == 0.0)
+        check(b.x != c.x)
+        check(b.y != c.y)
+        targets.forEach { (a, expected) ->
+            val actual = getPerpendicular(
+                a = a,
+                b = b,
+                c = c,
+            )
+            Assertions.assertEquals(expected, actual) { "a: $a" }
+        }
+    }
+
+    @Test
+    fun getPerpendicularZeroBTest() {
+        val targets = listOf(
+            pointOf(x = -5, y = 0) to pointOf(x = -4, y = 2),
+            pointOf(x = -3, y = -1) to pointOf(x = -2, y = 1),
+            pointOf(x = -3, y = 4) to pointOf(x = -4, y = 2),
+            pointOf(x = -1, y = 3) to pointOf(x = -2, y = 1),
+            pointOf(x = 1, y = 2) to pointOf(x = 0, y = 0),
+            pointOf(x = 3, y = 1) to pointOf(x = 2, y = -1),
+            pointOf(x = 5, y = 0) to pointOf(x = 4, y = -2),
+            pointOf(x = -1, y = -2) to pointOf(x = 0, y = 0),
+            pointOf(x = 1, y = -3) to pointOf(x = 2, y = -1),
+            pointOf(x = 3, y = -4) to pointOf(x = 4, y = -2),
+        )
+        check(targets.size == 10)
+        check(targets.toSet().size == targets.size)
+        val b = pointOf(x = 0, y = 0)
+        val c = pointOf(x = 4, y = -2)
+        check(b.x == 0.0)
+        check(b.x != c.x)
+        check(b.y != c.y)
+        targets.forEach { (a, expected) ->
+            val actual = getPerpendicular(
+                a = a,
+                b = b,
+                c = c,
+            )
+            Assertions.assertEquals(expected, actual) { "a: $a" }
+        }
+    }
+
+    @Test
+    fun getPerpendicularTest() {
+        val targets = listOf(
+            pointOf(x = 5, y = 8) to pointOf(x = 4, y = 6),
+            pointOf(x = 7, y = 7) to pointOf(x = 6, y = 5),
+            pointOf(x = 9, y = 6) to pointOf(x = 8, y = 4),
+            pointOf(x = 3, y = 4) to pointOf(x = 4, y = 6),
+            pointOf(x = 5, y = 3) to pointOf(x = 6, y = 5),
+            pointOf(x = 7, y = 2) to pointOf(x = 8, y = 4),
+        )
+        check(targets.size == 6)
+        check(targets.toSet().size == targets.size)
+        targets.forEach { (a, expected) ->
+            val b = pointOf(x = 4, y = 6)
+            val c = pointOf(x = 8, y = 4)
+            val actual = getPerpendicular(
+                a = a,
+                b = b,
+                c = c,
+            )
+            Assertions.assertEquals(expected, actual) { "a: $a" }
+        }
     }
 
     @Test
@@ -23,34 +95,45 @@ internal class PointCalculationsPerpendicularTest {
         val a = pointOf(x = 3, y = 2)
         val b = pointOf(x = 1, y = 1)
         val c = pointOf(x = 1, y = 3)
+        check(b.x == c.x)
+        check(b.y != c.y)
         val p = getPerpendicular(
             a = a,
             b = b,
             c = c,
         )
-        Assertions.assertEquals(p.y, a.y)
-        Assertions.assertEquals(p.x, b.x)
-        Assertions.assertEquals(p.x, c.x)
-        Assertions.assertEquals(p.y, a.y)
-        Assertions.assertEquals(p.x, 1.0)
-        Assertions.assertEquals(p.y, 2.0)
+        Assertions.assertEquals(a.y, p.y)
+        Assertions.assertEquals(b.x, p.x)
+        Assertions.assertEquals(c.x, p.x)
+        Assertions.assertEquals(a.y, p.y)
+        Assertions.assertEquals(1.0, p.x)
+        Assertions.assertEquals(2.0, p.y)
     }
 
     @Test
     fun getPerpendicularBCYTest() {
-        val a = pointOf(x = 2, y = 3)
-        val b = pointOf(x = 1, y = 1)
-        val c = pointOf(x = 3, y = 1)
-        val p = getPerpendicular(
-            a = a,
-            b = b,
-            c = c,
+        val targets = listOf(
+            pointOf(x = 0, y = 1) to pointOf(x = 0, y = 0),
+            pointOf(x = 1, y = 1) to pointOf(x = 1, y = 0),
+            pointOf(x = 2, y = 1) to pointOf(x = 2, y = 0),
+            pointOf(x = 0, y = -1) to pointOf(x = 0, y = 0),
+            pointOf(x = 1, y = -1) to pointOf(x = 1, y = 0),
+            pointOf(x = 2, y = -1) to pointOf(x = 2, y = 0),
         )
-        Assertions.assertEquals(p.x, a.x)
-        Assertions.assertEquals(p.y, b.y)
-        Assertions.assertEquals(p.y, c.y)
-        Assertions.assertEquals(p.x, 2.0)
-        Assertions.assertEquals(p.y, 1.0)
+        check(targets.size == 6)
+        check(targets.toSet().size == targets.size)
+        val b = pointOf(x = 0, y = 0)
+        val c = pointOf(x = 2, y = 0)
+        check(b.x != c.x)
+        check(b.y == c.y)
+        targets.forEach { (a, expected) ->
+            val actual = getPerpendicular(
+                a = a,
+                b = b,
+                c = c,
+            )
+            Assertions.assertEquals(expected, actual)
+        }
     }
 
     @Test
@@ -63,7 +146,7 @@ internal class PointCalculationsPerpendicularTest {
             b = b,
             c = c,
         )
-        Assertions.assertEquals(p, a)
+        Assertions.assertEquals(a, p)
     }
 
     @Test
@@ -72,10 +155,10 @@ internal class PointCalculationsPerpendicularTest {
         val c = pointOf(x = 5, y = 3)
         val p = getPerpendicular(
             a = a,
-            b = c,
+            b = a,
             c = c,
         )
-        Assertions.assertEquals(p, a)
+        Assertions.assertEquals(a, p)
     }
 
     @Test
@@ -85,9 +168,9 @@ internal class PointCalculationsPerpendicularTest {
         val p = getPerpendicular(
             a = a,
             b = b,
-            c = b,
+            c = a,
         )
-        Assertions.assertEquals(p, a)
+        Assertions.assertEquals(a, p)
     }
 
     @Test
@@ -98,6 +181,6 @@ internal class PointCalculationsPerpendicularTest {
             b = a,
             c = a,
         )
-        Assertions.assertEquals(p, a)
+        Assertions.assertEquals(a, p)
     }
 }
