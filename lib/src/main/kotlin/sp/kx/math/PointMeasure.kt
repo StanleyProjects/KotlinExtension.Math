fcab49da305b664fcbc6b755efe9c35705f45220
Stanley Wintergreen <kepocnhh@gmail.com>
2024-01-16 16:30:54 +0300

    doc point measure

--- lib/src/main/kotlin/sp/kx/math/PointMeasure.kt
+++ lib/src/main/kotlin/sp/kx/math/PointMeasure.kt
@@ -2,6 +2,27 @@ package sp.kx.math
 
 import sp.kx.math.measure.Measure
 
+/**
+ * Usage:
+ * ```
+ * val measure = measureOf(magnitude = 2.0)
+ * val point = pointOf(x = 1.0, y = 2.0, measure = measure)
+ *
+ *   ^
+ *   |
+ * 3 -
+ *   |
+ * 2 -               *
+ *   |
+ * 1 -
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return An instance of [Point] built from the [Double] values and the [Measure].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.7.2
+ */
 fun pointOf(
     x: Double,
     y: Double,
