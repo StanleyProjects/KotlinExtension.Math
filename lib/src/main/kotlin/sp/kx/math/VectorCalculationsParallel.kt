c621f68a6075449e6ea3c0152bd3a83700d77965
Stanley Wintergreen <kepocnhh@gmail.com>
2024-01-24 18:17:47 +0300

    doc vectors parallel

--- lib/src/main/kotlin/sp/kx/math/VectorCalculationsParallel.kt
+++ lib/src/main/kotlin/sp/kx/math/VectorCalculationsParallel.kt
@@ -1,5 +1,57 @@
 package sp.kx.math
 
+/**
+ * Usage:
+ * ```
+ * val ab = pointOf(x = 2, y = 3) + pointOf(x = 4, y = 3)
+ * val c = pointOf(x = 1, y = 1)
+ * val d = pointOf(x = 3, y = 1)
+ * val isParallel = ab.isParallel(
+ *     c = c,
+ *     d = d,
+ * )
+ * assertTrue(isParallel)
+ *
+ *   ^
+ *   |        a       b
+ * 3 -       *-------*
+ *   |
+ * 2 -
+ *   |    c       d
+ * 1 -   *-------*
+ *   |
+ * 0 *---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * Or:
+ * ```
+ * val ab = pointOf(x = 2, y = 2) + pointOf(x = 4, y = 3)
+ * val c = pointOf(x = 1, y = 1)
+ * val d = pointOf(x = 3, y = 1)
+ * val isParallel = ab.isParallel(
+ *     c = c,
+ *     d = d,
+ * )
+ * assertFalse(isParallel)
+ *
+ *   ^
+ *   |                b
+ * 3 -               *
+ *   |        a
+ * 2 -       *
+ *   |    c       d
+ * 1 -   *-------*
+ *   |
+ * 0 *---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return `true` if the segment described by [this] receiver is parallel to
+ * the segment described by the points [c] and [d];
+ * `false` otherwise.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.7.3
+ */
 fun Vector.isParallel(
     c: Point,
     d: Point,
@@ -16,6 +68,50 @@ fun Vector.isParallel(
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val ab = pointOf(x = 2, y = 3) + pointOf(x = 4, y = 3)
+ * val cd = pointOf(x = 1, y = 1) + pointOf(x = 3, y = 1)
+ * val isParallel = ab.isParallel(cd)
+ * assertTrue(isParallel)
+ *
+ *   ^
+ *   |        a       b
+ * 3 -       *-------*
+ *   |
+ * 2 -
+ *   |    c       d
+ * 1 -   *-------*
+ *   |
+ * 0 *---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * Or:
+ * ```
+ * val ab = pointOf(x = 2, y = 2) + pointOf(x = 4, y = 3)
+ * val cd = pointOf(x = 1, y = 1) + pointOf(x = 3, y = 1)
+ * val isParallel = ab.isParallel(cd)
+ * assertFalse(isParallel)
+ *
+ *   ^
+ *   |                b
+ * 3 -               *
+ *   |        a
+ * 2 -       *
+ *   |    c       d
+ * 1 -   *-------*
+ *   |
+ * 0 *---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return `true` if the segment described by [this] receiver is parallel to
+ * the segment described by [other] vector;
+ * `false` otherwise.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.7.3
+ */
 fun Vector.isParallel(other: Vector): Boolean {
     return isParallel(
         aX = start.x,
@@ -29,6 +125,60 @@ fun Vector.isParallel(other: Vector): Boolean {
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val a = pointOf(x = 2, y = 3)
+ * val b = pointOf(x = 4, y = 3)
+ * val cd = pointOf(x = 1, y = 1) + pointOf(x = 3, y = 1)
+ * val isParallel = isParallel(
+ *     a = a,
+ *     b = b,
+ *     cd = cd,
+ * )
+ * assertTrue(isParallel)
+ *
+ *   ^
+ *   |        a       b
+ * 3 -       *-------*
+ *   |
+ * 2 -
+ *   |    c       d
+ * 1 -   *-------*
+ *   |
+ * 0 *---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * Or:
+ * ```
+ * val a = pointOf(x = 2, y = 2)
+ * val b = pointOf(x = 4, y = 3)
+ * val cd = pointOf(x = 1, y = 1) + pointOf(x = 3, y = 1)
+ * val isParallel = isParallel(
+ *     a = a,
+ *     b = b,
+ *     cd = cd,
+ * )
+ * assertFalse(isParallel)
+ *
+ *   ^
+ *   |                b
+ * 3 -               *
+ *   |        a
+ * 2 -       *
+ *   |    c       d
+ * 1 -   *-------*
+ *   |
+ * 0 *---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ * @return `true` if the segment described by the points [a] and [b] is parallel to
+ * the segment described by [cd] vector;
+ * `false` otherwise.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.7.3
+ */
 fun isParallel(
     a: Point,
     b: Point,
