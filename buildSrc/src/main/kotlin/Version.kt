957c6b259ed62cb65721ba5a3813e0c50bfc728d
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-10 17:14:51 +0300

    jacoco:0.8.12

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,7 +1,7 @@
 object Version {
     const val detekt = "1.23.4"
     const val dokka = "1.9.10"
-    const val jacoco = "0.8.11"
+    const val jacoco = "0.8.12"
     const val jupiter = "5.10.1"
     const val jvmTarget = "17"
     const val kotlin = "1.9.24"
