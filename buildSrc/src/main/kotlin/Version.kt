a281bd76d793a70af9fba2bb0dd9545337b65b84
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-10 17:13:36 +0300

    kotlin:1.9.24

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -4,6 +4,6 @@ object Version {
     const val jacoco = "0.8.11"
     const val jupiter = "5.10.1"
     const val jvmTarget = "17"
-    const val kotlin = "1.9.20"
+    const val kotlin = "1.9.24"
     const val ktlint = "0.50.0"
 }
