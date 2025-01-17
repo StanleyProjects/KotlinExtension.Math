23643e2c2b27ae77f2d5580320bfdad55c270091
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-17 17:11:56 +0300

    measure doc

--- lib/src/main/kotlin/sp/kx/math/OffsetMeasure.kt
+++ lib/src/main/kotlin/sp/kx/math/OffsetMeasure.kt
@@ -2,7 +2,31 @@ package sp.kx.math
 
 import sp.kx.math.measure.Measure
 
-// todo doc
+/**
+ * Usage:
+ * ```
+ * val measure = measureOf(magnitude = 2.0)
+ * val offset = offsetOf(dX = 1.5, dY = 1.0, measure = measure)
+ * assertEquals(3.0, offset.dX)
+ * assertEquals(2.0, offset.dY)
+ * ```
+ *
+ * ```
+ *   ^
+ *   |
+ * 3 -
+ *   |
+ *dY -           *
+ *   |
+ * 1 -
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   dX  4
+ * ```
+ * @return An instance of [Offset] built from the [Double] values and the [Measure].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.8.0
+ */
 fun offsetOf(
     dX: Double,
     dY: Double,
--- lib/src/main/kotlin/sp/kx/math/SizeMeasure.kt
+++ lib/src/main/kotlin/sp/kx/math/SizeMeasure.kt
@@ -2,7 +2,31 @@ package sp.kx.math
 
 import sp.kx.math.measure.Measure
 
-// todo doc
+/**
+ * Usage:
+ * ```
+ * val measure = measureOf(magnitude = 2.0)
+ * val size = sizeOf(width = 1.5, height = 1.0, measure = measure)
+ * assertEquals(3.0, size.width)
+ * assertEquals(2.0, size.height)
+ * ```
+ *
+ * ```
+ *   ^
+ *   |
+ * 3 -
+ *   |
+ * h -           *
+ *   |
+ * 1 -
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   w   4
+ * ```
+ * @return An instance of [Size] built from the [Double] values and the [Measure].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.8.0
+ */
 fun sizeOf(
     width: Double,
     height: Double,
