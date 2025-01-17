eea7f0f91e813cefd5bfad47dc1471ac40023448
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-17 15:25:11 +0300

    mut test

--- lib/src/test/kotlin/sp/kx/math/MutablePointTest.kt
+++ lib/src/test/kotlin/sp/kx/math/MutablePointTest.kt
@@ -1,9 +1,11 @@
 package sp.kx.math
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-@Suppress("MagicNumber")
 internal class MutablePointTest {
     @Test
     fun constructorTest() {
@@ -11,27 +13,27 @@ internal class MutablePointTest {
             x = 1.2,
             y = 3.4,
         )
-        Assertions.assertNotEquals(actual.x, actual.y)
-        Assertions.assertEquals(1.2, actual.x)
-        Assertions.assertEquals(3.4, actual.y)
+        assertNotEquals(actual.x, actual.y)
+        assertEquals(1.2, actual.x)
+        assertEquals(3.4, actual.y)
         actual.x = 5.6
-        Assertions.assertEquals(5.6, actual.x)
-        Assertions.assertEquals(3.4, actual.y)
+        assertEquals(5.6, actual.x)
+        assertEquals(3.4, actual.y)
         actual.y = 7.8
-        Assertions.assertEquals(5.6, actual.x)
-        Assertions.assertEquals(7.8, actual.y)
+        assertEquals(5.6, actual.x)
+        assertEquals(7.8, actual.y)
     }
 
     @Test
     fun toStringTest() {
         MutablePoint(x = 1.2, y = 5.6).also { actual ->
-            Assertions.assertEquals("{x: 1.20, y: 5.60}", actual.toString())
+            assertEquals("{x: 1.20, y: 5.60}", actual.toString())
         }
         MutablePoint(x = 1.23, y = 5.67).also { actual ->
-            Assertions.assertEquals("{x: 1.23, y: 5.67}", actual.toString())
+            assertEquals("{x: 1.23, y: 5.67}", actual.toString())
         }
         MutablePoint(x = 1.234, y = 5.678).also { actual ->
-            Assertions.assertEquals("{x: 1.23, y: 5.68}", actual.toString())
+            assertEquals("{x: 1.23, y: 5.68}", actual.toString())
         }
     }
 
@@ -45,8 +47,8 @@ internal class MutablePointTest {
             x = 1.2,
             y = 3.4,
         )
-        Assertions.assertEquals(first, second)
-        Assertions.assertTrue(first == second)
+        assertEquals(first, second)
+        assertTrue(first == second)
     }
 
     @Test
@@ -55,18 +57,17 @@ internal class MutablePointTest {
             x = 1.2,
             y = 3.4,
         )
-        Assertions.assertNotEquals(MutablePoint(1.2, 1.2), actual)
-        Assertions.assertFalse(MutablePoint(1.2, 1.2) == actual)
-        Assertions.assertNotEquals(MutablePoint(3.4, 3.4), actual)
-        Assertions.assertFalse(MutablePoint(3.4, 3.4) == actual)
-        Assertions.assertNotEquals(MutablePoint(3.4, 1.2), actual)
-        Assertions.assertFalse(MutablePoint(3.4, 1.2) == actual)
-        Assertions.assertFalse(actual.equals(Unit))
+        assertNotEquals(MutablePoint(1.2, 1.2), actual)
+        assertFalse(MutablePoint(1.2, 1.2) == actual)
+        assertNotEquals(MutablePoint(3.4, 3.4), actual)
+        assertFalse(MutablePoint(3.4, 3.4) == actual)
+        assertNotEquals(MutablePoint(3.4, 1.2), actual)
+        assertFalse(MutablePoint(3.4, 1.2) == actual)
+        assertFalse(actual.equals(Unit))
     }
 
     @Test
     fun hashCodeTest() {
-        @Suppress("IgnoredReturnValue")
         MutablePoint(x = 1.2, y = 3.4).hashCode()
     }
 
@@ -76,13 +77,13 @@ internal class MutablePointTest {
             x = 1.2,
             y = 3.4,
         )
-        Assertions.assertNotEquals(actual.x, actual.y)
-        Assertions.assertEquals(1.2, actual.x)
-        Assertions.assertEquals(3.4, actual.y)
+        assertNotEquals(actual.x, actual.y)
+        assertEquals(1.2, actual.x)
+        assertEquals(3.4, actual.y)
         actual.set(x = 5.6, y = 7.8)
-        Assertions.assertNotEquals(actual.x, actual.y)
-        Assertions.assertEquals(5.6, actual.x)
-        Assertions.assertEquals(7.8, actual.y)
+        assertNotEquals(actual.x, actual.y)
+        assertEquals(5.6, actual.x)
+        assertEquals(7.8, actual.y)
     }
 
     @Test
@@ -91,13 +92,13 @@ internal class MutablePointTest {
             x = 1.2,
             y = 3.4,
         )
-        Assertions.assertNotEquals(foo.x, foo.y)
-        Assertions.assertEquals(1.2, foo.x)
-        Assertions.assertEquals(3.4, foo.y)
+        assertNotEquals(foo.x, foo.y)
+        assertEquals(1.2, foo.x)
+        assertEquals(3.4, foo.y)
         val bar = pointOf(x = 5.6, y = 7.8)
-        Assertions.assertNotEquals(foo, bar)
+        assertNotEquals(foo, bar)
         foo.set(bar)
-        Assertions.assertEquals(foo, bar)
+        assertEquals(foo, bar)
     }
 
     @Test
@@ -106,11 +107,42 @@ internal class MutablePointTest {
             x = 1.2,
             y = 3.4,
         )
-        Assertions.assertNotEquals(actual.x, actual.y)
-        Assertions.assertEquals(1.2, actual.x)
-        Assertions.assertEquals(3.4, actual.y)
+        assertNotEquals(actual.x, actual.y)
+        assertEquals(1.2, actual.x)
+        assertEquals(3.4, actual.y)
         actual.swap()
-        Assertions.assertEquals(3.4, actual.x)
-        Assertions.assertEquals(1.2, actual.y)
+        assertEquals(3.4, actual.x)
+        assertEquals(1.2, actual.y)
+    }
+
+    @Test
+    fun mutTest() {
+        val delta = 0.00000001
+        listOf(
+            -9.0 to 1.2,
+            -1.2 to 2.4,
+            0.0 to 0.0,
+            1.0 to 1.0,
+            1.2 to 3.4,
+            5.6 to -7.8,
+            5.6 to 7.8,
+        ).forEach { (x, y) ->
+            val expected = pointOf(x = x, y = y)
+            val actual = expected.mut()
+            check(x == expected.x)
+            check(y == expected.y)
+            val message = """
+                expected: $expected
+                actual: $actual
+                delta: $delta (${delta.toString(24)})
+            """.trimIndent()
+            assertEquals(actual::class.java, MutablePoint::class.java, message)
+            assertEquals(x, actual.x, delta, message)
+            assertEquals(y, actual.y, delta, message)
+            assertEquals(expected, actual, message)
+            val value = 128.0
+            actual.x = value
+            assertEquals(value, actual.x, delta, message)
+        }
     }
 }
