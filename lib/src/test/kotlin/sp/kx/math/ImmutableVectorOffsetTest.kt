191e7f34d3d515e04701ca203bad489746302771
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-20 19:21:48 +0300

    vectors test

--- lib/src/test/kotlin/sp/kx/math/ImmutableVectorOffsetTest.kt
+++ lib/src/test/kotlin/sp/kx/math/ImmutableVectorOffsetTest.kt
@@ -1,9 +1,10 @@
 package sp.kx.math
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-@Suppress("MagicNumber")
 internal class ImmutableVectorOffsetTest {
     @Test
     fun vectorOfTest() {
@@ -17,13 +18,13 @@ internal class ImmutableVectorOffsetTest {
             dX = dX,
             dY = dY,
         )
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2 + dX, actual.start.x)
-        Assertions.assertEquals(3.4 + dY, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(5.6 + dX, actual.finish.x)
-        Assertions.assertEquals(7.8 + dY, actual.finish.y)
+        assertNotEquals(actual.start, actual.finish)
+        assertNotEquals(actual.start.x, actual.start.y)
+        assertEquals(1.2 + dX, actual.start.x)
+        assertEquals(3.4 + dY, actual.start.y)
+        assertNotEquals(actual.finish.x, actual.finish.y)
+        assertEquals(5.6 + dX, actual.finish.x)
+        assertEquals(7.8 + dY, actual.finish.y)
     }
 
     @Test
@@ -36,13 +37,13 @@ internal class ImmutableVectorOffsetTest {
             finishY = 7.8,
             offset = offset,
         )
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2 + offset.dX, actual.start.x)
-        Assertions.assertEquals(3.4 + offset.dY, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(5.6 + offset.dX, actual.finish.x)
-        Assertions.assertEquals(7.8 + offset.dY, actual.finish.y)
+        assertNotEquals(actual.start, actual.finish)
+        assertNotEquals(actual.start.x, actual.start.y)
+        assertEquals(1.2 + offset.dX, actual.start.x)
+        assertEquals(3.4 + offset.dY, actual.start.y)
+        assertNotEquals(actual.finish.x, actual.finish.y)
+        assertEquals(5.6 + offset.dX, actual.finish.x)
+        assertEquals(7.8 + offset.dY, actual.finish.y)
     }
 
     @Test
@@ -57,15 +58,15 @@ internal class ImmutableVectorOffsetTest {
             dX = dX,
             dY = dY,
         )
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2 + dX, actual.start.x)
-        Assertions.assertEquals(3.4 + dY, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(5.6 + dX, actual.finish.x)
-        Assertions.assertEquals(7.8 + dY, actual.finish.y)
-        Assertions.assertEquals(finish.x + dX, actual.finish.x)
-        Assertions.assertEquals(finish.y + dY, actual.finish.y)
+        assertNotEquals(actual.start, actual.finish)
+        assertNotEquals(actual.start.x, actual.start.y)
+        assertEquals(1.2 + dX, actual.start.x)
+        assertEquals(3.4 + dY, actual.start.y)
+        assertNotEquals(actual.finish.x, actual.finish.y)
+        assertEquals(5.6 + dX, actual.finish.x)
+        assertEquals(7.8 + dY, actual.finish.y)
+        assertEquals(finish.x + dX, actual.finish.x)
+        assertEquals(finish.y + dY, actual.finish.y)
     }
 
     @Test
@@ -78,15 +79,15 @@ internal class ImmutableVectorOffsetTest {
             finish = finish,
             offset = offset,
         )
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2 + offset.dX, actual.start.x)
-        Assertions.assertEquals(3.4 + offset.dY, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(5.6 + offset.dX, actual.finish.x)
-        Assertions.assertEquals(7.8 + offset.dY, actual.finish.y)
-        Assertions.assertEquals(finish.x + offset.dX, actual.finish.x)
-        Assertions.assertEquals(finish.y + offset.dY, actual.finish.y)
+        assertNotEquals(actual.start, actual.finish)
+        assertNotEquals(actual.start.x, actual.start.y)
+        assertEquals(1.2 + offset.dX, actual.start.x)
+        assertEquals(3.4 + offset.dY, actual.start.y)
+        assertNotEquals(actual.finish.x, actual.finish.y)
+        assertEquals(5.6 + offset.dX, actual.finish.x)
+        assertEquals(7.8 + offset.dY, actual.finish.y)
+        assertEquals(finish.x + offset.dX, actual.finish.x)
+        assertEquals(finish.y + offset.dY, actual.finish.y)
     }
 
     @Test
@@ -94,18 +95,18 @@ internal class ImmutableVectorOffsetTest {
         val foo = pointOf(x = 1.2, y = 3.4)
         val offset = offsetOf(dX = 12.8, dY = 25.6)
         val actual: Vector = foo.toVector(offset = offset)
-        Assertions.assertEquals(actual.start, foo)
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2, actual.start.x)
-        Assertions.assertEquals(3.4, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(1.2 + 12.8, actual.finish.x)
-        Assertions.assertEquals(3.4 + 25.6, actual.finish.y)
-        Assertions.assertEquals(1.2 + offset.dX, actual.finish.x)
-        Assertions.assertEquals(3.4 + offset.dY, actual.finish.y)
-        Assertions.assertEquals(actual.start.x + offset.dX, actual.finish.x)
-        Assertions.assertEquals(actual.start.y + offset.dY, actual.finish.y)
+        assertEquals(actual.start, foo)
+        assertNotEquals(actual.start, actual.finish)
+        assertNotEquals(actual.start.x, actual.start.y)
+        assertEquals(1.2, actual.start.x)
+        assertEquals(3.4, actual.start.y)
+        assertNotEquals(actual.finish.x, actual.finish.y)
+        assertEquals(1.2 + 12.8, actual.finish.x)
+        assertEquals(3.4 + 25.6, actual.finish.y)
+        assertEquals(1.2 + offset.dX, actual.finish.x)
+        assertEquals(3.4 + offset.dY, actual.finish.y)
+        assertEquals(actual.start.x + offset.dX, actual.finish.x)
+        assertEquals(actual.start.y + offset.dY, actual.finish.y)
     }
 
     @Test
@@ -114,16 +115,42 @@ internal class ImmutableVectorOffsetTest {
         val foo = pointOf(x = 1.2, y = 3.4)
         val bar = pointOf(x = 5.6, y = 7.8)
         val actual: Vector = foo.toVector(finish = bar, offset = offset)
-        Assertions.assertNotEquals(actual.start, actual.finish)
-        Assertions.assertNotEquals(actual.start.x, actual.start.y)
-        Assertions.assertEquals(1.2 + offset.dX, actual.start.x)
-        Assertions.assertEquals(3.4 + offset.dY, actual.start.y)
-        Assertions.assertNotEquals(actual.finish.x, actual.finish.y)
-        Assertions.assertEquals(5.6 + offset.dX, actual.finish.x)
-        Assertions.assertEquals(7.8 + offset.dY, actual.finish.y)
-        Assertions.assertEquals(foo.x + offset.dX, actual.start.x)
-        Assertions.assertEquals(foo.y + offset.dY, actual.start.y)
-        Assertions.assertEquals(bar.x + offset.dX, actual.finish.x)
-        Assertions.assertEquals(bar.y + offset.dY, actual.finish.y)
+        assertNotEquals(actual.start, actual.finish)
+        assertNotEquals(actual.start.x, actual.start.y)
+        assertEquals(1.2 + offset.dX, actual.start.x)
+        assertEquals(3.4 + offset.dY, actual.start.y)
+        assertNotEquals(actual.finish.x, actual.finish.y)
+        assertEquals(5.6 + offset.dX, actual.finish.x)
+        assertEquals(7.8 + offset.dY, actual.finish.y)
+        assertEquals(foo.x + offset.dX, actual.start.x)
+        assertEquals(foo.y + offset.dY, actual.start.y)
+        assertEquals(bar.x + offset.dX, actual.finish.x)
+        assertEquals(bar.y + offset.dY, actual.finish.y)
+    }
+
+    @Test
+    fun toOffsetTest() {
+        val delta = 0.0001
+        val points = 4
+        listOf(
+            vectorOf(-1.0, -1.0, 0.0, 0.0) to offsetOf(1.0, 1.0),
+            vectorOf(0.0, 0.0, 0.0, 0.0) to offsetOf(0.0, 0.0),
+            vectorOf(0.0, 0.0, 1.0, 1.0) to offsetOf(1.0, 1.0),
+            vectorOf(1.0, 1.0, 0.0, 0.0) to offsetOf(-1.0, -1.0),
+            vectorOf(1.0, 1.0, 1.0, 1.0) to offsetOf(0.0, 0.0),
+            vectorOf(1.2, 3.4, 5.6, 7.8) to offsetOf(4.4, 4.4),
+        ).forEach { (vector, expected) ->
+            val actual = vector.toOffset()
+            val message = """
+                vector: $vector (${vector.toString(24)})
+                expected: $expected (${expected.toString(24)})
+                actual: $actual (${actual.toString(24)})
+                delta: $delta (${delta.toString(24)})
+                points: $points
+            """.trimIndent()
+            assertEquals(expected.dX, actual.dX, delta, message)
+            assertEquals(expected.dY, actual.dY, delta, message)
+            assertTrue(expected.eq(actual, points = points), message)
+        }
     }
 }
