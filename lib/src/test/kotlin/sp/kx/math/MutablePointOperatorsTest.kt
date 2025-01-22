9192e083a1653a3a5677765d545f12e537a76fa5
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-22 16:57:40 +0300

    mut offset operators test

--- lib/src/test/kotlin/sp/kx/math/MutablePointOperatorsTest.kt
+++ lib/src/test/kotlin/sp/kx/math/MutablePointOperatorsTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 internal class MutablePointOperatorsTest {
     @Test
     fun timesAssignTest() {
-        val point = pointOf(4.2, 6.4)
+        val initial = pointOf(4.2, 6.4)
         val points = 8
         val delta = 0.00000001
         listOf(
@@ -18,25 +18,15 @@ internal class MutablePointOperatorsTest {
             1.5 to pointOf(6.3, 9.6),
             2.0 to pointOf(8.4, 12.8),
         ).forEach { (multiplier, expected) ->
-            val actual = MutablePoint(x = point.x, y = point.y)
+            val actual = MutablePoint(x = initial.x, y = initial.y)
             actual *= multiplier
-            val message = """
-                point: $point
-                multiplier: ${multiplier.toString(24)}
-                expected: $expected
-                actual: $actual
-                points: $points
-                delta: ${delta.toString(24)}
-            """.trimIndent()
-            assertEquals(expected.x, actual.x, delta, message)
-            assertEquals(expected.y, actual.y, delta, message)
-            assertTrue(expected.eq(other = actual, points = points), message)
+            assert(expected = expected, actual = actual, points = points, delta = delta)
         }
     }
 
     @Test
     fun divAssignTest() {
-        val point = pointOf(4.2, 6.4)
+        val initial = pointOf(4.2, 6.4)
         val points = 8
         val delta = 0.00000001
         listOf(
@@ -47,25 +37,15 @@ internal class MutablePointOperatorsTest {
             0.5 to pointOf(8.4, 12.8),
             (2.0 / 3.0) to pointOf(6.3, 9.6),
         ).forEach { (multiplier, expected) ->
-            val actual = MutablePoint(x = point.x, y = point.y)
+            val actual = MutablePoint(x = initial.x, y = initial.y)
             actual /= multiplier
-            val message = """
-                point: $point
-                multiplier: ${multiplier.toString(24)}
-                expected: $expected
-                actual: $actual
-                points: $points
-                delta: ${delta.toString(24)}
-            """.trimIndent()
-            assertEquals(expected.x, actual.x, delta, message)
-            assertEquals(expected.y, actual.y, delta, message)
-            assertTrue(expected.eq(other = actual, points = points), message)
+            assert(expected = expected, actual = actual, points = points, delta = delta)
         }
     }
 
     @Test
     fun plusAssignTest() {
-        val point = pointOf(4.2, 6.4)
+        val initial = pointOf(4.2, 6.4)
         val points = 8
         val delta = 0.00000001
         listOf(
@@ -78,25 +58,15 @@ internal class MutablePointOperatorsTest {
             1.5 to pointOf(5.7, 7.9),
             2.0 to pointOf(6.2, 8.4),
         ).forEach { (value, expected) ->
-            val actual = MutablePoint(x = point.x, y = point.y)
+            val actual = MutablePoint(x = initial.x, y = initial.y)
             actual += value
-            val message = """
-                point: $point
-                value: ${value.toString(24)}
-                expected: $expected
-                actual: $actual
-                points: $points
-                delta: ${delta.toString(24)}
-            """.trimIndent()
-            assertEquals(expected.x, actual.x, delta, message)
-            assertEquals(expected.y, actual.y, delta, message)
-            assertTrue(expected.eq(other = actual, points = points), message)
+            assert(expected = expected, actual = actual, points = points, delta = delta)
         }
     }
 
     @Test
     fun minusAssignTest() {
-        val point = pointOf(4.2, 6.4)
+        val initial = pointOf(4.2, 6.4)
         val points = 8
         val delta = 0.00000001
         listOf(
@@ -109,25 +79,15 @@ internal class MutablePointOperatorsTest {
             -1.5 to pointOf(5.7, 7.9),
             -2.0 to pointOf(6.2, 8.4),
         ).forEach { (value, expected) ->
-            val actual = MutablePoint(x = point.x, y = point.y)
+            val actual = MutablePoint(x = initial.x, y = initial.y)
             actual -= value
-            val message = """
-                point: $point
-                value: ${value.toString(24)}
-                expected: $expected
-                actual: $actual
-                points: $points
-                delta: ${delta.toString(24)}
-            """.trimIndent()
-            assertEquals(expected.x, actual.x, delta, message)
-            assertEquals(expected.y, actual.y, delta, message)
-            assertTrue(expected.eq(other = actual, points = points), message)
+            assert(expected = expected, actual = actual, points = points, delta = delta)
         }
     }
 
     @Test
     fun plusAssignOffsetTest() {
-        val point = pointOf(1.2, 3.4)
+        val initial = pointOf(1.2, 3.4)
         val points = 8
         val delta = 0.00000001
         listOf(
@@ -140,25 +100,15 @@ internal class MutablePointOperatorsTest {
             offsetOf(1.0, 1.0) to pointOf(2.2, 4.4),
             offsetOf(2.8, 0.6) to pointOf(4.0, 4.0),
         ).forEach { (offset, expected) ->
-            val actual = MutablePoint(x = point.x, y = point.y)
+            val actual = MutablePoint(x = initial.x, y = initial.y)
             actual += offset
-            val message = """
-                point: $point
-                offset: $offset
-                expected: $expected
-                actual: $actual
-                points: $points
-                delta: ${delta.toString(24)}
-            """.trimIndent()
-            assertEquals(expected.x, actual.x, delta, message)
-            assertEquals(expected.y, actual.y, delta, message)
-            assertTrue(expected.eq(other = actual, points = points), message)
+            assert(expected = expected, actual = actual, points = points, delta = delta)
         }
     }
 
     @Test
     fun minusAssignOffsetTest() {
-        val point = pointOf(1.2, 3.4)
+        val initial = pointOf(1.2, 3.4)
         val points = 8
         val delta = 0.00000001
         listOf(
@@ -171,11 +121,15 @@ internal class MutablePointOperatorsTest {
             offsetOf(-1.0, -1.0) to pointOf(2.2, 4.4),
             offsetOf(-2.8, -0.6) to pointOf(4.0, 4.0),
         ).forEach { (offset, expected) ->
-            val actual = MutablePoint(x = point.x, y = point.y)
+            val actual = MutablePoint(x = initial.x, y = initial.y)
             actual -= offset
+            assert(expected = expected, actual = actual, points = points, delta = delta)
+        }
+    }
+
+    companion object {
+        private fun assert(expected: Point, actual: Point, points: Int, delta: Double) {
             val message = """
-                point: $point
-                offset: $offset
                 expected: $expected
                 actual: $actual
                 points: $points
