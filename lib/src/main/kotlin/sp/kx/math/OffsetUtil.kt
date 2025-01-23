8310686a308e6617f9be5075fe25d2128e86f470
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-23 17:01:52 +0300

    offset util doc

--- lib/src/main/kotlin/sp/kx/math/OffsetUtil.kt
+++ lib/src/main/kotlin/sp/kx/math/OffsetUtil.kt
@@ -139,11 +139,11 @@ fun Offset.reversed(): Offset {
  * Usage:
  * ```
  * val foo = offsetOf(dX = 3.0, dY = 4.0)
- * val bar = sizeOf(width = 1.0, height = 2.0)
- * val baz = foo.plus(size = bar, multiplier = 2.0)
- * assertFalse(foo === baz)
- * assertEquals(5.0, baz.dX)
- * assertEquals(8.0, baz.dY)
+ * val size = sizeOf(width = 1.0, height = 2.0)
+ * val bar = foo.plus(size = size, multiplier = 2.0)
+ * assertFalse(foo === bar)
+ * assertEquals(5.0, bar.dX)
+ * assertEquals(8.0, bar.dY)
  * ```
  *
  * Special case:
@@ -175,6 +175,33 @@ fun Offset.plus(size: Size, multiplier: Double): Offset {
     return offsetOf(dX = dX + size.width * multiplier, dY = dY + size.height * multiplier)
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = offsetOf(dX = 1.0, dY = 2.0)
+ * val bar = foo.plus(dX = 2.0, dY = 1.0)
+ * assertFalse(foo === bar)
+ * assertEquals(3.0, bar.dX)
+ * assertEquals(3.0, bar.dY)
+ * ```
+ *
+ * ```
+ *   ^
+ *   |
+ * 3 -           * bar
+ *   |
+ * 2 -   * foo
+ *   |
+ * 1 -
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * @return A new [Offset] object with a copy of [this] receiver's [Offset.dX] and [Offset.dY] with the [dX] and [dY] offsets added to them.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.8.1
+ */
 fun Offset.plus(dX: Double, dY: Double): Offset {
     return offsetOf(
         dX = this.dX + dX,
@@ -182,6 +209,38 @@ fun Offset.plus(dX: Double, dY: Double): Offset {
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = offsetOf(dX = 1.0, dY = 1.0)
+ * val size = sizeOf(width = 1.0, height = 2.0)
+ * val bar = foo.plus(dX = 2.0, dY = 1.0)
+ * assertFalse(foo === bar)
+ * assertEquals(4.0, bar.dX)
+ * assertEquals(4.0, bar.dY)
+ * ```
+ *
+ * ```
+ *   ^
+ *   |
+ * 4 -               * bar
+ *   |
+ * 3 -
+ *   |
+ * 2 -
+ *   |
+ * 1 -   * foo
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * @return A new [Offset] object with a copy of [this] receiver's [Offset.dX] and [Offset.dY]
+ * with [size]'s [Size.width] and [Size.height] and
+ * with the [dX] and [dY] offsets added to them.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.8.1
+ */
 fun Offset.plus(size: Size, dX: Double, dY: Double): Offset {
     return offsetOf(
         dX = this.dX + size.width + dX,
@@ -189,6 +248,34 @@ fun Offset.plus(size: Size, dX: Double, dY: Double): Offset {
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = offsetOf(dX = 1.0, dY = 1.0)
+ * val bar = foo.plus(dX = 1.0, dY = 1.0, multiplier = 2.0)
+ * assertFalse(foo === bar)
+ * assertEquals(3.0, bar.dX)
+ * assertEquals(3.0, bar.dY)
+ * ```
+ *
+ * ```
+ *   ^
+ *   |
+ * 3 -           * bar
+ *   |
+ * 2 -
+ *   |
+ * 1 -   * foo
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * @return A new [Offset] object with a copy of [this] receiver's [Offset.dX] and [Offset.dY]
+ * with the [dX] and [dY] offsets multiplied by the [multiplier] added to them.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.8.1
+ */
 fun Offset.plus(dX: Double, dY: Double, multiplier: Double): Offset {
     return offsetOf(
         dX = this.dX + dX * multiplier,
@@ -196,6 +283,38 @@ fun Offset.plus(dX: Double, dY: Double, multiplier: Double): Offset {
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val foo = offsetOf(dX = 0.0, dY = 0.0)
+ * val size = sizeOf(width = 1.0, height = 1.0)
+ * val bar = foo.plus(size = size, dX = 1.0, dY = 1.0, multiplier = 2.0)
+ * assertFalse(foo === bar)
+ * assertEquals(4.0, bar.dX)
+ * assertEquals(4.0, bar.dY)
+ * ```
+ *
+ * ```
+ *   ^
+ *   |
+ * 4 -               * bar
+ *   |
+ * 3 -
+ *   |
+ * 2 -
+ *   |
+ * 1 -
+ *   |
+ * 0 +---|---|---|---|--->
+ *   0   1   2   3   4
+ * ```
+ *
+ * @return A new [Offset] object with a copy of [this] receiver's [Offset.dX] and [Offset.dY]
+ * with [size]'s [Size.width] and [Size.height] and
+ * with the [dX] and [dY] offsets multiplied by the [multiplier] added to them.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.8.1
+ */
 fun Offset.plus(size: Size, dX: Double, dY: Double, multiplier: Double): Offset {
     return offsetOf(
         dX = this.dX + (size.width + dX) * multiplier,
