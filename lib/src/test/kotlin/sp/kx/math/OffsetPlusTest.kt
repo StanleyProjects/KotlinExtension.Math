93bba4fa6252eaecc2acee8de1fce8e340fef391
Stanley Wintergreen <kepocnhh@gmail.com>
2025-01-23 16:50:12 +0300

    offset util test

--- lib/src/test/kotlin/sp/kx/math/OffsetPlusTest.kt
+++ lib/src/test/kotlin/sp/kx/math/OffsetPlusTest.kt
@@ -5,17 +5,72 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class OffsetPlusTest {
-    private class Env(
-        val offset: Offset,
-        val size: Size,
-        val actual: Offset,
-        val expected: Offset,
-    )
+    @Test
+    fun plusDoublesTest() {
+        val points = 8
+        val delta = 0.00000001
+        val issues = listOf(
+            -1.0 to -1.0,
+            -1.0 to 0.0,
+            0.0 to -1.0,
+            0.0 to 0.0,
+            0.0 to 1.0,
+            1.0 to -1.0,
+            1.0 to 0.0,
+            1.0 to 1.0,
+            1.2 to 3.4,
+            5.6 to 7.8,
+        )
+        issues.forEach { o ->
+            val offset = offsetOf(dX = o.first, dY = o.second)
+            issues.forEach { (dX, dY) ->
+                val actual = offset.plus(dX = dX, dY = dY)
+                val expected = offsetOf(dX = offset.dX + dX, dY = offset.dY + dY)
+                assert(expected = expected, actual = actual, points = points, delta = delta)
+            }
+        }
+    }
+
+    @Test
+    fun plusDoublesMultiplierTest() {
+        val points = 8
+        val delta = 0.00000001
+        val issues = listOf(
+            -1.0 to -1.0,
+            -1.0 to 0.0,
+            0.0 to -1.0,
+            0.0 to 0.0,
+            0.0 to 1.0,
+            1.0 to -1.0,
+            1.0 to 0.0,
+            1.0 to 1.0,
+            1.2 to 3.4,
+            5.6 to 7.8,
+        )
+        val multipliers = setOf(
+            -1.0,
+            0.0,
+            0.5,
+            1.0,
+            1.2,
+            2.0,
+        )
+        issues.forEach { o ->
+            val offset = offsetOf(dX = o.first, dY = o.second)
+            issues.forEach { (dX, dY) ->
+                multipliers.forEach { multiplier ->
+                    val actual = offset.plus(dX = dX, dY = dY, multiplier = multiplier)
+                    val expected = offsetOf(dX = offset.dX + dX * multiplier, dY = offset.dY + dY * multiplier)
+                    assert(expected = expected, actual = actual, points = points, delta = delta)
+                }
+            }
+        }
+    }
 
     @Test
     fun plusSizeTest() {
-        val delta = 0.0001
-        val points = 4
+        val points = 8
+        val delta = 0.00000001
         val issues = listOf(
             -1.0 to -1.0,
             -1.0 to 0.0,
@@ -34,26 +89,44 @@ internal class OffsetPlusTest {
                 val size = sizeOf(width = s.first, height = s.second)
                 val actual = offset + size
                 val expected = offsetOf(dX = offset.dX + size.width, dY = offset.dY + size.height)
-                val message = """
-                    offset: $offset (${offset.toString(24)})
-                    size: $size (${size.toString(24)})
-                    actual: $actual (${actual.toString(24)})
-                    expected: $expected (${expected.toString(24)})
-                    delta: $delta (${delta.toString(24)})
-                    points: $points
-                """.trimIndent()
-                assertEquals(actual.dX, expected.dX, delta, message)
-                assertEquals(actual.dY, expected.dY, delta, message)
-                assertEquals(expected, actual, message)
-                assertTrue(expected.eq(other = actual, points = points), message)
+                assert(expected = expected, actual = actual, points = points, delta = delta)
+            }
+        }
+    }
+
+    @Test
+    fun plusSizeDoublesTest() {
+        val points = 8
+        val delta = 0.00000001
+        val issues = listOf(
+            -1.0 to -1.0,
+            -1.0 to 0.0,
+            0.0 to -1.0,
+            0.0 to 0.0,
+            0.0 to 1.0,
+            1.0 to -1.0,
+            1.0 to 0.0,
+            1.0 to 1.0,
+            1.2 to 3.4,
+            5.6 to 7.8,
+        )
+        issues.forEach { o ->
+            val offset = offsetOf(dX = o.first, dY = o.second)
+            issues.forEach { s ->
+                val size = sizeOf(width = s.first, height = s.second)
+                issues.forEach { (dX, dY) ->
+                    val actual = offset.plus(size = size, dX = dX, dY = dY)
+                    val expected = offsetOf(dX = offset.dX + size.width + dX, dY = offset.dY + size.height + dY)
+                    assert(expected = expected, actual = actual, points = points, delta = delta)
+                }
             }
         }
     }
 
     @Test
     fun plusSizeMultiplierTest() {
-        val delta = 0.0001
-        val points = 4
+        val points = 8
+        val delta = 0.00000001
         val issues = listOf(
             -1.0 to -1.0,
             -1.0 to 0.0,
@@ -81,20 +154,65 @@ internal class OffsetPlusTest {
                 multipliers.forEach { multiplier ->
                     val actual = offset.plus(size = size, multiplier = multiplier)
                     val expected = offsetOf(dX = offset.dX + size.width * multiplier, dY = offset.dY + size.height * multiplier)
-                    val message = """
-                        offset: $offset (${offset.toString(24)})
-                        size: $size (${size.toString(24)})
-                        actual: $actual (${actual.toString(24)})
-                        expected: $expected (${expected.toString(24)})
-                        delta: $delta (${delta.toString(24)})
-                        points: $points
-                    """.trimIndent()
-                    assertEquals(actual.dX, expected.dX, delta, message)
-                    assertEquals(actual.dY, expected.dY, delta, message)
-                    assertEquals(expected, actual, message)
-                    assertTrue(expected.eq(other = actual, points = points), message)
+                    assert(expected = expected, actual = actual, points = points, delta = delta)
                 }
             }
+        }
+    }
+
+    @Test
+    fun plusSizeDoublesMultiplierTest() {
+        val points = 8
+        val delta = 0.00000001
+        val issues = listOf(
+            -1.0 to -1.0,
+            -1.0 to 0.0,
+            0.0 to -1.0,
+            0.0 to 0.0,
+            0.0 to 1.0,
+            1.0 to -1.0,
+            1.0 to 0.0,
+            1.0 to 1.0,
+            1.2 to 3.4,
+            5.6 to 7.8,
+        )
+        val multipliers = setOf(
+            -1.0,
+            0.0,
+            0.5,
+            1.0,
+            1.2,
+            2.0,
+        )
+        issues.forEach { o ->
+            val offset = offsetOf(dX = o.first, dY = o.second)
+            issues.forEach { s ->
+                val size = sizeOf(width = s.first, height = s.second)
+                issues.forEach { (dX, dY) ->
+                    multipliers.forEach { multiplier ->
+                        val actual = offset.plus(size = size, dX = dX, dY = dY, multiplier = multiplier)
+                        val expected = offsetOf(
+                            dX = offset.dX + (size.width + dX) * multiplier,
+                            dY = offset.dY + (size.height + dY) * multiplier,
+                        )
+                        assert(expected = expected, actual = actual, points = points, delta = delta)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private fun assert(expected: Offset, actual: Offset, points: Int, delta: Double) {
+            val message = """
+                expected: $expected
+                actual: $actual
+                points: $points
+                delta: ${delta.toString(24)}
+            """.trimIndent()
+            assertEquals(expected.dX, actual.dX, delta, message)
+            assertEquals(expected.dY, actual.dY, delta, message)
+            assertTrue(expected.eq(other = actual, points = points), message)
         }
     }
 }
